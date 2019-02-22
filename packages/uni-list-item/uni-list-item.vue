8a421709d01e3cdd473466fd74633048879739f7
hulinneil <1289739946@qq.com>
2019-02-22 12:07:59 +0800

    更新list的选择器名称

--- packages/uni-list-item/uni-list-item.vue
+++ packages/uni-list-item/uni-list-item.vue
@@ -1,18 +1,18 @@
 <template>
-	<view class="uni-list-cell" :class="[disabled === true || disabled === 'true' ? 'uni-list-cell--disabled' : '']"
-	 :hover-class="disabled === true || disabled === 'true' || showSwitch === true || showSwitch === 'true' ? '' : 'uni-list-cell--hover'" @click="onClick">
-		<view class="uni-list-cell__container">
-			<view class="uni-list-cell__icon" v-if="thumb">
-				<image class="uni-list-cell__icon-img" :src="thumb"></image>
+	<view class="uni-list-item" :class="[disabled === true || disabled === 'true' ? 'uni-list-item--disabled' : '']"
+	 :hover-class="disabled === true || disabled === 'true' || showSwitch === true || showSwitch === 'true' ? '' : 'uni-list-item--hover'" @click="onClick">
+		<view class="uni-list-item__container">
+			<view class="uni-list-item__icon" v-if="thumb">
+				<image class="uni-list-item__icon-img" :src="thumb"></image>
 			</view>
-			<view class="uni-list-cell__icon" v-else-if="showExtraIcon === true || showExtraIcon === 'true'">
+			<view class="uni-list-item__icon" v-else-if="showExtraIcon === true || showExtraIcon === 'true'">
 				<uni-icon :color="extraIcon.color" :size="extraIcon.size" :type="extraIcon.type"></uni-icon>
 			</view>
-			<view class="uni-list-cell__content">
-				<view class="uni-list-cell__content-title">{{title}}</view>
-				<view class="uni-list-cell__content-note" v-if="note">{{note}}</view>
+			<view class="uni-list-item__content">
+				<view class="uni-list-item__content-title">{{title}}</view>
+				<view class="uni-list-item__content-note" v-if="note">{{note}}</view>
 			</view>
-			<view class="uni-list-cell__extra" v-if="showBadge === true || showBadge === 'true' || showArrow === true || showArrow === 'true'||showSwitch === true || showSwitch === 'true'">
+			<view class="uni-list-item__extra" v-if="showBadge === true || showBadge === 'true' || showArrow === true || showArrow === 'true'||showSwitch === true || showSwitch === 'true'">
 				<uni-badge v-if="showBadge === true || showBadge === 'true'" :type="badgeType" :text="badgeText"></uni-badge>
 				<switch v-if="showSwitch === true || showSwitch === 'true'" :disabled='disabled' :checked="switchChecked" @change="onSwitchChange" />
 				<uni-icon v-if="showArrow === true || showArrow === 'true'" color="#bbb" size="20" type="arrowright"></uni-icon>
@@ -99,9 +99,9 @@
 		opacity: 0.3;
 	}
 
-	$list-cell-pd:$uni-spacing-col-lg $uni-spacing-row-lg;
+	$list-item-pd:$uni-spacing-col-lg $uni-spacing-row-lg;
 
-	.uni-list-cell {
+	.uni-list-item {
 		font-size: $uni-font-size-lg;
 		position: relative;
 		display: flex;
@@ -118,7 +118,7 @@
 		}
 
 		&__container {
-			padding: $list-cell-pd;
+			padding: $list-item-pd;
 			width: 100%;
 			box-sizing: border-box;
 			flex: 1;
@@ -190,7 +190,7 @@
 		}
 	}
 
-	.uni-list>.uni-list-cell:last-child .uni-list-cell-container:after {
+	.uni-list>.uni-list-item:last-child .uni-list-item-container:after {
 		height: 0px;
 	}
 </style>
