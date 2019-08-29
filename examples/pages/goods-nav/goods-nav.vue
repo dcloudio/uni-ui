eebc6927b3eb846f229b4b593821216a0fed26d1
mehaotian <490272692@qq.com>
2019-08-29 11:34:23 +0800

    feat: goods-nav

--- examples/pages/goods-nav/goods-nav.vue
+++ examples/pages/goods-nav/goods-nav.vue
@@ -1,9 +1,28 @@
 <template>
-  <view class="content">
-    <uni-goods-nav
-      :options="options"
-      :button-group="buttonGroup"
-      @add="add"/>
+  <view>
+    <view class="example-info">
+      uni-goods-nav 组件主要用于电商类应用底部导航，可自定义加入购物车，购买等操作
+    </view>
+    <view class="example-title">基础用法</view>
+    <view class="example-body">
+      <uni-goods-nav />
+    </view>
+
+    <view class="example-title">自定义用法</view>
+    <view class="example-body">
+      <uni-goods-nav
+        :fill="true"
+        :options="options"
+        :button-group="buttonGroup"
+        @click="onClick"
+        @buttonClick="buttonClick" />
+    </view>
+    <view class="goods-carts">
+      <uni-goods-nav
+        :options="options"
+        :button-group="buttonGroup"/>
+    </view>
+
   </view>
 </template>
 
@@ -11,39 +30,53 @@
 export default {
   data () {
     return {
-      options:[{
-        icon:'https://img-cdn-qiniu.dcloud.net.cn/uniapp/uni-ui/goodsnav/kefu.png',
-        text:'客服'
-      },{
-        icon:'https://img-cdn-qiniu.dcloud.net.cn/uniapp/uni-ui/goodsnav/dianpu.png',
-        text:'店铺'
-      },{
-        icon:'https://img-cdn-qiniu.dcloud.net.cn/uniapp/uni-ui/goodsnav/carts.png',
-        text:'购物车',
-        info:2,
+      options: [{
+        icon: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/uni-ui/goodsnav/kefu.png',
+        text: '客服'
+      }, {
+        icon: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/uni-ui/goodsnav/dianpu.png',
+        text: '店铺'
+      }, {
+        icon: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/uni-ui/goodsnav/carts.png',
+        text: '购物车',
+        info: 2
       }],
-      buttonGroup:[
-        {
-          text:'加入购物车',
-          backgroundColor:'#ff0000',
-          color:'#fff'
-        },
-        {
-          text:'立即购买',
-          backgroundColor:'#ffa200',
-          color:'#fff'
-        }
+      buttonGroup: [{
+        text: '加入购物车',
+        backgroundColor: '#ff0000',
+        color: '#fff'
+      },
+      {
+        text: '立即购买',
+        backgroundColor: '#ffa200',
+        color: '#fff'
+      }
       ]
     }
   },
   onLoad () {},
   methods: {
-    add (e) {
-      console.log(e);
-      this.options[2].info ++
+    onClick (e) {
+      uni.showToast({
+        title: `点击${e.content.text}`,
+        icon: 'none'
+      })
+    },
+    buttonClick (e) {
+      console.log(e)
+      this.options[2].info++
     }
   }
 }
 </script>
 
-<style></style>
+<style>
+  .example-body {
+    padding: 0;
+  }
+  .goods-carts {
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+  }
+</style>
--- packages/uni-goods-nav/uni-goods-nav.vue
+++ packages/uni-goods-nav/uni-goods-nav.vue
@@ -1,14 +1,20 @@
 <template>
   <view>
     <!-- 底部占位 -->
-    <view class="uni-tab__seat"/>
+    <view class="uni-tab__seat" />
     <view class="uni-tab__cart-box flex">
       <view class="flex uni-tab__cart-sub-box">
-        <view v-for="(item,index) in options" :key="index" class="flex uni-tab__cart-button-left uni-tab__shop-cart">
+        <view
+          v-for="(item,index) in options"
+          :key="index"
+          class="flex uni-tab__cart-button-left uni-tab__shop-cart"
+          @click="onClick(index,item)">
           <view class="uni-tab__icon">
-            <image :src="item.icon" mode="widthFix"></image>
+            <image
+              :src="item.icon"
+              mode="widthFix"/></image>
           </view>
-          <text class="uni-tab__text">{{item.text}}</text>
+          <text class="uni-tab__text">{{ item.text }}</text>
           <view class="flex uni-tab__dot-box">
             <view
               v-if="item.info"
@@ -19,13 +25,17 @@
           </view>
         </view>
       </view>
-      <view class="flex uni-tab__cart-sub-box uni-tab__right">
-        <view v-for="(item,index) in buttonGroup" :key="index"
-          class="flex uni-tab__cart-button-right"
+      <view
+        :class="{'uni-tab__right':fill}"
+        class="flex uni-tab__cart-sub-box ">
+        <view
+          v-for="(item,index) in buttonGroup"
+          :key="index"
           :style="{backgroundColor:item.backgroundColor,color:item.color}"
-          @click="add(index,item)">{{item.text}}</view>
+          class="flex uni-tab__cart-button-right"
+          @click="buttonClick(index,item)">{{ item.text }}</view>
 
-        <!-- <view class="flex uni-tab__cart-button-right uni-tab__color-y ">立即购买</view> -->
+          <!-- <view class="flex uni-tab__cart-button-right uni-tab__color-y ">立即购买</view> -->
       </view>
     </view>
   </view>
@@ -37,22 +47,48 @@ export default {
   props: {
     options: {
       type: Array,
-      default(){
-        return []
+      default () {
+        return [{
+          icon: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/uni-ui/goodsnav/dianpu.png',
+          text: '店铺'
+        }, {
+          icon: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/uni-ui/goodsnav/carts.png',
+          text: '购物车'
+        }]
       }
     },
-    buttonGroup:{
+    buttonGroup: {
       type: Array,
-      default(){
-        return []
+      default () {
+        return [{
+          text: '加入购物车',
+          backgroundColor: '#ff0000',
+          color: '#fff'
+        },
+        {
+          text: '立即购买',
+          backgroundColor: '#ffa200',
+          color: '#fff'
+        }
+        ]
       }
+    },
+    fill: {
+      type: Boolean,
+      default: false
     }
   },
   methods: {
-    add (index,item) {
-      this.$emit('add',{
+    onClick (index, item) {
+      this.$emit('click', {
         index,
-        content:item
+        content: item
+      })
+    },
+    buttonClick (index, item) {
+      this.$emit('buttonClick', {
+        index,
+        content: item
       })
     }
   }
@@ -60,97 +96,99 @@ export default {
 </script>
 
 <style>
-.uni-tab__seat{
-	width: 100%;
-	height: 100upx;
-}
-.flex {
-	display: flex;
-}
-.uni-tab__cart-box {
-	position: fixed;
-	bottom: 0;
-	left: 0;
-	width: 100%;
-	height: 100upx;
-	background: #fff;
-	border-top: #f5f5f5 1px solid;
-  z-index: 900;
-}
-.uni-tab__cart-sub-box {
-	width: 100%;
-	box-sizing: border-box;
-}
-.uni-tab__right {
-  margin: 5px 0;
-  margin-right: 10px;
-  border-radius: 100px;
-  overflow: hidden;
-}
-.uni-tab__cart-button-left {
-	position: relative;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-	width: 100%;
-	font-size: 12px;
-}
+  .flex {
+    display: flex;
+  }
 
-.uni-tab__icon {
-  width: 20px;
-  height: 20px;
-}
-.uni-tab__icon image {
-  width: 100%;
-  height: 100%;
-}
+  .uni-tab__cart-box {
+    width: 100%;
+    height: 100upx;
+    background: #fff;
+    z-index: 900;
+  }
 
-.uni-tab__cart-button-left .uni-tab__text {
-	margin-top: 5upx;
-  font-size: 12px;
-	color: #666;
-}
-.uni-tab__cart-button-right {
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	font-size: 12upx;
-	color: #fff;
-}
-.uni-tab__cart-button-right:active {
-  opacity: 0.7;
-}
-.uni-tab__cart-button-left .uni-tab__dot-box {
-	position: absolute;
-	right: 40upx;
-	top: 20upx;
-	justify-content: center;
-	align-items: center;
-	width: 0;
-	height: 0;
-}
-.uni-tab__dot-box .uni-tab__dot {
-	flex-shrink: 0;
-	width: 30upx;
-	height: 30upx;
-	line-height: 30upx;
-	color: #ffffff;
-	text-align: center;
-  font-size: 12px;
-	background: #ff0000;
-	border-radius: 50%;
-}
-.uni-tab__dot-box .uni-tab__dot.uni-tab__dots {
-	padding: 0 8upx;
-	width: auto;
-	border-radius: 30upx;
-}
+  .uni-tab__cart-sub-box {
+    width: 100%;
+    box-sizing: border-box;
+  }
 
-.uni-tab__color-y {
-	background: #ffa200;
-}
-.uni-tab__color-r {
-	background: #ff0000;
-}
+  .uni-tab__right {
+    margin: 5px 0;
+    margin-right: 10px;
+    border-radius: 100px;
+    overflow: hidden;
+  }
 
+  .uni-tab__cart-button-left {
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    font-size: 12px;
+  }
+
+  .uni-tab__icon {
+    width: 20px;
+    height: 20px;
+  }
+
+  .uni-tab__icon image {
+    width: 100%;
+    height: 100%;
+  }
+
+  .uni-tab__cart-button-left .uni-tab__text {
+    margin-top: 5upx;
+    font-size: 12px;
+    color: #666;
+  }
+
+  .uni-tab__cart-button-right {
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    font-size: 12upx;
+    color: #fff;
+  }
+
+  .uni-tab__cart-button-right:active {
+    opacity: 0.7;
+  }
+
+  .uni-tab__cart-button-left .uni-tab__dot-box {
+    position: absolute;
+    right: 40upx;
+    top: 20upx;
+    justify-content: center;
+    align-items: center;
+    width: 0;
+    height: 0;
+  }
+
+  .uni-tab__dot-box .uni-tab__dot {
+    flex-shrink: 0;
+    width: 30upx;
+    height: 30upx;
+    line-height: 30upx;
+    color: #ffffff;
+    text-align: center;
+    font-size: 12px;
+    background: #ff0000;
+    border-radius: 50%;
+  }
+
+  .uni-tab__dot-box .uni-tab__dot.uni-tab__dots {
+    padding: 0 8upx;
+    width: auto;
+    border-radius: 30upx;
+  }
+
+  .uni-tab__color-y {
+    background: #ffa200;
+  }
+
+  .uni-tab__color-r {
+    background: #ff0000;
+  }
 </style>
