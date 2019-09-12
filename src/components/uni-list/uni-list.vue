b97915832054d24651a2db414f038487f0212f1a
mehaotian <490272692@qq.com>
2019-09-12 13:03:41 +0800

    fix: uni-title

--- src/components/uni-list/uni-list.vue
+++ src/components/uni-list/uni-list.vue
@@ -1,45 +1,47 @@
 <template>
   <view class="uni-list">
-    <slot/>
+    <slot />
   </view>
 </template>
 <script>
-export default {
-  name: 'UniList'
-}
+  export default {
+    name: 'UniList'
+  }
 </script>
 <style lang="scss">
-	.uni-list {
-		background-color: $uni-bg-color;
-		position: relative;
-		width: 100%;
-		display: flex;
-		flex-direction: column;
+  @import '~@/uni.scss';
 
-		&:after {
-			position: absolute;
-			z-index: 10;
-			right: 0;
-			bottom: 0;
-			left: 0;
-			height: 1px;
-			content: '';
-			-webkit-transform: scaleY(0.5);
-			transform: scaleY(0.5);
-			background-color: $uni-border-color;
-		}
+  .uni-list {
+    background-color: $uni-bg-color;
+    position: relative;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
 
-		&:before {
-			position: absolute;
-			z-index: 10;
-			right: 0;
-			top: 0;
-			left: 0;
-			height: 1px;
-			content: '';
-			-webkit-transform: scaleY(0.5);
-			transform: scaleY(0.5);
-			background-color: $uni-border-color;
-		}
-	}
+    &:after {
+      position: absolute;
+      z-index: 10;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      height: 1px;
+      content: '';
+      -webkit-transform: scaleY(0.5);
+      transform: scaleY(0.5);
+      background-color: $uni-border-color;
+    }
+
+    &:before {
+      position: absolute;
+      z-index: 10;
+      right: 0;
+      top: 0;
+      left: 0;
+      height: 1px;
+      content: '';
+      -webkit-transform: scaleY(0.5);
+      transform: scaleY(0.5);
+      background-color: $uni-border-color;
+    }
+  }
 </style>
--- src/components/uni-title/uni-title.vue
+++ src/components/uni-title/uni-title.vue
@@ -4,8 +4,8 @@
       <view :class="type" class="uni-title__head-tag" />
     </view>
     <view class="uni-title__content">
-      <view :class="{'distraction':!subTitle}" class="uni-title__content-title">{{ title }}</view>
-      <view class="uni-title__content-sub">{{ subTitle }}</view>
+      <text :class="{'distraction':!subTitle}" class="uni-title__content-title">{{ title }}</text>
+      <text class="uni-title__content-sub">{{ subTitle }}</text>
     </view>
   </view>
 </template>
@@ -54,62 +54,66 @@
 </script>
 
 <style lang="scss">
+  @import '~@/uni.scss';
   .uni-title {
     margin-top: 10px;
-    z-index: 100;
-    display: flex;
+    flex-direction: row;
     align-items: center;
     padding: 5px 10px;
-    width: 100%;
     height: 50px;
     background-color: #fdfdfd;
-    border-bottom: 1px #f5f5f5 solid;
-    box-sizing: border-box;
+    border-bottom-color: #f5f5f5;
+    border-bottom-style: solid;
+    border-bottom-width: 1px;
+  }
 
-    &.sticky {
-      position: sticky;
-      // top: 44px;
-    }
+  .sticky {
+    position: sticky;
+    // top: 44px;
+  }
 
-    &__head {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-right: 10px; 
+  .uni-title__head {
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-right: 10px;
+  }
 
-      &-tag {
-        &.line {
-          height: 15px;
-          background: #ccc;
-          border-radius: 5px;
-          width: 3px;
-        }
+  .uni-title__head-tag {}
 
-        &.circle {
-          width: 8px;
-          height: 8px;
-          border-radius: 50%;
-          background: #ccc;
-        }
-      }
-    }
+  .line {
+    height: 15px;
+    background-color: #ccc;
+    border-radius: 5px;
+    width: 3px;
+  }
 
-    &__content {
-      color: #464e52;
+  .circle {
+    width: 8px;
+    height: 8px;
+    // border-radius: 50px;
+    border-top-right-radius: 50px;
+    border-top-left-radius: 50px;
+    border-bottom-left-radius: 50px;
+    border-bottom-right-radius: 50px;
+    background-color: #ccc;
+  }
 
-      &-title {
-        font-size: 16px;
+  .uni-title__content {
+    color: #464e52;
+  }
 
-        &.distraction {
-          display: flex;
-          align-items: center;
-        }
-      }
+  .uni-title__content-title {
+    font-size: 16px;
+  }
 
-      &-sub {
-        font-size: 12px;
-        color: #999;
-      }
-    }
+  .distraction {
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .uni-title__content-sub {
+    font-size: 12px;
+    color: #999;
   }
 </style>
