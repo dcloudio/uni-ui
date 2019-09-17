9b43a603f2f306d2c40dea209d8ff08f63b2924d
wangyaqi <wangyaqi988@163.com>
2019-09-17 10:18:50 +0800

    update: uni-steps nvue初始化

--- src/components/uni-steps/uni-steps.vue
+++ src/components/uni-steps/uni-steps.vue
@@ -1,257 +1,170 @@
 <template>
   <view class="uni-steps">
-    <view
-      :class="'uni-steps-' + direction"
-      class="uni-steps-items">
-      <view
-        v-for="(item, index) in options"
-        :key="index"
-        :class="{ 'uni-steps-process': index === active, 'uni-steps-finish': index < active }"
-        class="uni-steps-item">
-        <view
-          :style="{ color: index === active ? activeColor : '' }"
-          class="uni-steps-item-title-container">
-          <view class="uni-steps-item-title">{{ item.title }}</view>
-          <view
-            v-if="item.desc"
-            class="uni-steps-item-desc">{{ item.desc }}</view>
+    <view class="uni-steps__row" :class="{'uni-steps__column':direction==='column'}">
+      <view class="uni-steps__row-text-container" :class="{'uni-steps__column-text-container':direction==='column'}">
+        <view v-for="(item,index) in options" :key="index" :class="{'uni-steps__column-text':direction==='column'}" class="uni-steps__row-text">
+          <text :style="{color:index<=active?activeColor:deactiveColor}" :class="{'uni-steps__column-title':direction==='column'}" class="uni-steps__row-title">{{item.title}}</text>
+          <text :style="{color:index<=active?activeColor:deactiveColor}" :class="{'uni-steps__column-desc':direction==='column'}" class="uni-steps__row-desc">{{item.desc}}</text>
         </view>
-        <view class="uni-steps-item-circle-container">
-          <view
-            v-if="index !== active"
-            :style="{ backgroundColor: index < active ? activeColor : '' }"
-            class="uni-steps-item-circle" />
-          <uni-icons
-            v-else
-            :color="activeColor"
-            type="checkbox-filled"
-            size="14" />
+      </view>
+      <view class="uni-steps__row-container">
+        <view class="uni-steps__row-line-item" v-for="(item,index) in options" :key="index">
+          <view :style="{backgroundColor:index<=active&&index!==0?activeColor:index===0?'transparent':deactiveColor}"
+            class="uni-steps__row-line uni-steps__row-line--before"></view>
+          <uni-icons v-if="index === active" :color="activeColor" class="uni-steps__row-check" type="checkbox-filled"
+            size="14"></uni-icons>
+          <view v-else :style="{backgroundColor:index<active?activeColor:deactiveColor}" class="uni-steps__row-circle"></view>
+          <view :style="{backgroundColor:index<active&&index!==options.length-1?activeColor:index===options.length-1?'transparent':deactiveColor}"
+            class="uni-steps__row-line uni-steps__row-line--after"></view>
         </view>
-        <view
-          v-if="index !== options.length - 1"
-          :style="{ backgroundColor: index < active ? activeColor : '' }"
-          class="uni-steps-item-line" />
       </view>
     </view>
   </view>
 </template>
 
 <script>
-import uniIcons from '../uni-icons/uni-icons.vue'
-export default {
-  name: 'UniSteps',
-  components: {
-    uniIcons
-  },
-  props: {
-    direction: {
-      // 排列方向 row column
-      type: String,
-      default: 'row'
+  import uniIcons from '../uni-icons/uni-icons.vue'
+  export default {
+    name: 'UniSteps',
+    components: {
+      uniIcons
     },
-    activeColor: {
-      // 激活状态颜色
-      type: String,
-      default: '#1aad19'
+    props: {
+      direction: {
+        // 排列方向 row column
+        type: String,
+        default: 'row'
+      },
+      activeColor: {
+        // 激活状态颜色
+        type: String,
+        default: '#1aad19'
+      },
+      deactiveColor: {
+        // 未激活状态颜色
+        type: String,
+        default: '#999999'
+      },
+      active: {
+        // 当前步骤
+        type: Number,
+        default: 0
+      },
+      options: {
+        type: Array,
+        default () {
+          return []
+        }
+      } // 数据
     },
-    active: {
-      // 当前步骤
-      type: Number,
-      default: 0
-    },
-    options: {
-      type: Array,
-      default () {
-        return []
-      }
-    } // 数据
-  },
-  data () {
-    return {}
+    data() {
+      return {}
+    }
   }
-}
 </script>
 
 <style lang="scss">
-$uni-border-color-light: #ebedf0; //较浅的灰色，如steps时间轴的颜色
-.uni-steps {
-	width: 100%;
-	box-sizing: border-box;
-	display: flex;
-	flex-direction: column;
-	overflow: hidden;
-	position: relative;
+  @import '@/uni.scss';
 
-	&-items {
-		position: relative;
-		display: flex;
-		flex-direction: row;
-		margin: 10px;
-		box-sizing: border-box;
-		overflow: hidden;
+  .uni-steps {
+    /* #ifndef APP-NVUE */
+    display: flex;
+    width: 100%;
+    /* #endif */
+    /* #ifdef APP-NVUE */
+    flex: 1;
+    /* #endif */
+  }
 
-		&.uni-steps-column {
-			margin: 10px 0;
-			padding-left: 31px;
-			flex-direction: column;
+  .uni-steps__column {
+    flex-direction: row-reverse;
+  }
 
-			.uni-steps-item {
-				&:after {
-					content: ' ';
-					position: absolute;
-					height: 1px;
-					width: 100%;
-					bottom: 9px;
-					left: 0;
-					background-color: $uni-border-color-light;
-					transform: scaleY(0.5);
-				}
+  .uni-steps__row-text-container {
+    /* #ifndef APP-NVUE */
+    display: flex;
+    /* #endif */
+    flex-direction: row;
+  }
 
-				&:last-child {
-					position: relative;
+  .uni-steps__column-text-container {
+    flex-direction: column;
+    flex: 1;
+  }
 
-					&:after {
-						height: 0px;
-					}
+  .uni-steps__row-text {
+    /* #ifndef APP-NVUE */
+    display: inline-flex;
+    /* #endif */
+    flex: 1;
+    flex-direction: column;
+  }
+  
+  .uni-steps__column-text {
+    
+  }
 
-					.uni-steps-item-title-container {
-						text-align: left;
-					}
+  .uni-steps__row-title {
+    font-size: 14px;
+    line-height: 16px;
+    text-align: center;
+  }
+  
+  .uni-steps__column-title{
+    text-align: left;
+  }
 
-					.uni-steps-item-circle-container {
-						left: -17px;
-						right: auto;
-					}
-				}
+  .uni-steps__row-desc {
+    font-size: 12px;
+    line-height: 14px;
+    text-align: center;
+  }
+  
+  .uni-steps__column-desc{
+    text-align: left;
+  }
 
-				&-title-container {
-					transform: none;
-					display: block;
-					line-height: 36upx;
-				}
+  .uni-steps__row-container {
+    /* #ifndef APP-NVUE */
+    display: flex;
+    /* #endif */
+    flex-direction: row;
+    height: 14px;
+    line-height: 14px;
+  }
 
-				&-title {
-					text-overflow: ellipsis;
-					white-space: nowrap;
-					overflow: hidden;
-				}
+  .uni-steps__row-line-item {
+    /* #ifndef APP-NVUE */
+    display: inline-flex;
+    /* #endif */
+    flex-direction: row;
+    align-items: center;
+    flex: 1;
+  }
 
-				&-desc {
-					white-space: normal;
-					display: -webkit-box;
-					-webkit-box-orient: vertical;
-					-webkit-line-clamp: 2;
-					overflow: hidden;
-				}
+  .uni-steps__row-line {
+    flex: 1;
+    height: 1px;
+    background-color: $uni-text-color-grey;
+  }
 
-				&-circle-container {
-					left: -17px;
-					top: -1px;
-					bottom: auto;
-					padding: 8px 0px;
-					z-index: 1;
-				}
+  .uni-steps__row-line--after {
+    transform: translateX(1px);
+  }
 
-				&-line {
-					height: 100%;
-					width: 1px;
-					left: -15px;
-					top: -1px;
-					bottom: auto;
-				}
+  .uni-steps__row-line--before {
+    transform: translateX(-1px);
+  }
 
-				&.uni-steps-process {
-					.uni-steps-item-circle-container {
-						bottom: auto;
-						left: -21px;
-					}
-				}
-			}
-		}
-	}
+  .uni-steps__row-circle {
+    width: 5px;
+    height: 5px;
+    border-radius: 10px;
+    background-color: $uni-text-color-grey;
+    margin: 0px 3px;
+  }
 
-	&-item {
-		flex: 1;
-		position: relative;
-		padding-bottom: 18px;
-
-		&-title-container {
-			text-align: left;
-			margin-left: 3px;
-			display: inline-block;
-			transform: translateX(-50%);
-			color: $uni-text-color-grey;
-		}
-
-		&-title {
-			font-size: $uni-font-size-base;
-		}
-
-		&-desc {
-			font-size: $uni-font-size-sm;
-		}
-
-		&:first-child {
-			.uni-steps-item-title-container {
-				transform: none;
-				margin-left: 0;
-			}
-		}
-
-		&:last-child {
-			position: absolute;
-			right: 0;
-
-			.uni-steps-item-title-container {
-				transform: none;
-				text-align: right;
-			}
-
-			.uni-steps-item-circle-container {
-				left: auto;
-				right: -8px;
-			}
-		}
-
-		&-circle-container {
-			position: absolute;
-			bottom: 8px;
-			left: -8px;
-			padding: 0 8px;
-			background-color: $uni-bg-color;
-			z-index: 1;
-		}
-
-		&-circle {
-			width: 5px;
-			height: 5px;
-			background-color: $uni-text-color-grey;
-			border-radius: $uni-border-radius-circle;
-		}
-
-		&-line {
-			background-color: $uni-border-color-light;
-			position: absolute;
-			bottom: 10px;
-			left: 0;
-			width: 100%;
-			height: 1px;
-		}
-
-		&.uni-steps-finish {
-			.uni-steps-item-title-container {
-				color: $uni-text-color;
-			}
-		}
-
-		&.uni-steps-process {
-			.uni-steps-item-circle-container {
-				bottom: 3px;
-        height: 14px;
-        line-height: 14px;
-				display: flex;
-			}
-		}
-	}
-}
+  .uni-steps__row-check {
+    margin: 0px 6px;
+  }
 </style>
