312a6c0e54bbd5d0508b8e9ddc2147a82ac45c77
chenchen <458679677@qq.com>
2019-09-18 20:38:19 +0800

    fix:uni-nav-bar 调整三个元素的宽度，字体居中问题

--- src/components/uni-nav-bar/uni-nav-bar.vue
+++ src/components/uni-nav-bar/uni-nav-bar.vue
@@ -1,26 +1,30 @@
 <template>
   <view class="uni-navbar">
-    <view
-      :class="{ 'uni-navbar--fixed': fixed, 'uni-navbar--shadow': border, 'uni-navbar--border': border }"
-      :style="{ 'background-color': backgroundColor }"
-      class="uni-navbar__content"
-    >
-      <!-- <uni-status-bar v-if="statusBar" /> -->
+    <view :class="{ 'uni-navbar--fixed': fixed, 'uni-navbar--shadow': border, 'uni-navbar--border': border }" :style="{ 'background-color': backgroundColor }"
+      class="uni-navbar__content">
+      <uni-status-bar v-if="statusBar" />
       <view :style="{ color: color }" class="uni-navbar__header uni-navbar__content_view">
-        <view class="uni-navbar__header-btns uni-navbar__content_view" @tap="onClickLeft">
-          <view v-if="leftIcon.length" class="uni-navbar__content_view"><uni-icons :type="leftIcon" :color="color" size="24" /></view>
-          <view v-if="leftText.length" :class="{ 'uni-navbar-btn-icon-left': !leftIcon.length }" class="uni-navbar-btn-text uni-navbar__content_view"><text class="uni-nav-bar-text">{{ leftText }}</text></view>
+        <view class="uni-navbar__header-btns uni-navbar__header-btns-left uni-navbar__content_view" @tap="onClickLeft">
+          <view v-if="leftIcon.length" class="uni-navbar__content_view">
+            <uni-icons :type="leftIcon" :color="color" size="24" />
+          </view>
+          <view v-if="leftText.length" :class="{ 'uni-navbar-btn-icon-left': !leftIcon.length }" class="uni-navbar-btn-text uni-navbar__content_view"><text
+              class="uni-nav-bar-text">{{ leftText }}</text></view>
           <slot name="left" />
         </view>
-        <view class="uni-navbar__header-container uni-navbar__content_view" >
+        <view class="uni-navbar__header-container uni-navbar__content_view">
           <view v-if="title.length" class="uni-navbar__header-container-inner uni-navbar__content_view"><text class="uni-nav-bar-text">{{ title }}</text></view>
           <!-- 标题插槽 -->
           <slot />
         </view>
-        <view :class="title.length ? 'uni-navbar__header-btns-right' : ''" class="uni-navbar__header-btns uni-navbar__content_view" @tap="onClickRight">
-          <view v-if="rightIcon.length" class="uni-navbar__content_view"><uni-icons :type="rightIcon" :color="color" size="24" /></view>
+        <view :class="title.length ? 'uni-navbar__header-btns-right' : ''" class="uni-navbar__header-btns uni-navbar__content_view"
+          @tap="onClickRight">
+          <view v-if="rightIcon.length" class="uni-navbar__content_view">
+            <uni-icons :type="rightIcon" :color="color" size="24" />
+          </view>
           <!-- 优先显示图标 -->
-          <view v-if="rightText.length && !rightIcon.length" class="uni-navbar-btn-text uni-navbar__content_view"><text class="uni-nav-bar-text">{{ rightText }}</text></view>
+          <view v-if="rightText.length && !rightIcon.length" class="uni-navbar-btn-text uni-navbar__content_view"><text
+              class="uni-nav-bar-text">{{ rightText }}</text></view>
           <slot name="right" />
         </view>
       </view>
@@ -33,166 +37,154 @@
 </template>
 
 <script>
-import uniStatusBar from '@/components/uni-status-bar/uni-status-bar.vue';
-import uniIcons from '@/components/uni-icons/uni-icons.vue';
+  import uniStatusBar from '@/components/uni-status-bar/uni-status-bar.vue';
+  import uniIcons from '@/components/uni-icons/uni-icons.vue';
 
-export default {
-  name: 'UniNavBar',
-  components: {
-    uniStatusBar,
-    uniIcons
-  },
-  props: {
-    title: {
-      type: String,
-      default: ''
+  export default {
+    name: 'UniNavBar',
+    components: {
+      uniStatusBar,
+      uniIcons
     },
-    leftText: {
-      type: String,
-      default: ''
+    props: {
+      title: {
+        type: String,
+        default: ''
+      },
+      leftText: {
+        type: String,
+        default: ''
+      },
+      rightText: {
+        type: String,
+        default: ''
+      },
+      leftIcon: {
+        type: String,
+        default: ''
+      },
+      rightIcon: {
+        type: String,
+        default: ''
+      },
+      fixed: {
+        type: [Boolean, String],
+        default: false
+      },
+      color: {
+        type: String,
+        default: '#000000'
+      },
+      backgroundColor: {
+        type: String,
+        default: '#FFFFFF'
+      },
+      statusBar: {
+        type: [Boolean, String],
+        default: false
+      },
+      shadow: {
+        type: [String, Boolean],
+        default: true
+      },
+      border: {
+        type: [String, Boolean],
+        default: true
+      }
     },
-    rightText: {
-      type: String,
-      default: ''
-    },
-    leftIcon: {
-      type: String,
-      default: ''
-    },
-    rightIcon: {
-      type: String,
-      default: ''
-    },
-    fixed: {
-      type: [Boolean, String],
-      default: false
-    },
-    color: {
-      type: String,
-      default: '#000000'
-    },
-    backgroundColor: {
-      type: String,
-      default: '#FFFFFF'
-    },
-    statusBar: {
-      type: [Boolean, String],
-      default: false
-    },
-    shadow: {
-      type: [String, Boolean],
-      default: true
-    },
-    border: {
-      type: [String, Boolean],
-      default: true
+    methods: {
+      onClickLeft() {
+        this.$emit('clickLeft');
+      },
+      onClickRight() {
+        this.$emit('clickRight');
+      }
     }
-  },
-  methods: {
-    onClickLeft() {
-      this.$emit('clickLeft');
-    },
-    onClickRight() {
-      this.$emit('clickRight');
-    }
-  }
-};
+  };
 </script>
 
 <style lang="scss">
-@import '~@/uni.scss';
-$nav-height: 44px;
+  @import '~@/uni.scss';
+  $nav-height: 44px;
 
-.uni-nav-bar-text{
-  font-size: $uni-font-size-lg;
-}
-.uni-navbar {
-  width: 750rpx;
-}
-.uni-navbar__content {
-  position: relative;
-  // width: 100%;
-  width: 750rpx;
-  background-color: $uni-bg-color;
-  overflow: hidden;
-
-
-}
-.uni-navbar__content_view {
-    // line-height: $nav-height;
-    align-items: center;
-    flex-direction: row;
-    // width: 100%;
+  .uni-nav-bar-text {
+    font-size: $uni-font-size-lg;
   }
 
-.uni-navbar__header {
-  flex-direction: row;
-  width: 750rpx;
-  height: $nav-height;
-  line-height: $nav-height;
-  font-size: 16px;
-}
-.uni-navbar__header-btns {
-  flex-wrap: nowrap;
-  // flex-shrink: 0;
-  width: 120upx;
-  padding: 0 12upx;
-  justify-content: center;
-  align-items: center;
-}
-// .uni-navbar__header-btns:first-child {
-//   padding-left: 0;
-// }
-//
-// .uni-navbar__header-btns:last-child {
-//   width: 60upx;
-// }
-//
-// .uni-navbar__header-btns-right:last-child {
-//   width: 120rpx;
-//   text-align: right;
-//   flex-direction: row-reverse;
-// }
+  .uni-navbar {
+    width: 750rpx;
+  }
 
-.uni-navbar__header-container {
-  // width: 100%;
-  flex:1;
-  margin: 0 10upx;
-}
+  .uni-navbar__content {
+    position: relative;
+    width: 750rpx;
+    background-color: $uni-bg-color;
+    overflow: hidden;
 
-.uni-navbar__header-container-inner {
- // width: 750rpx;
- flex:1;
-  align-items: center;
-  justify-content: center;
-  font-size: 30upx;
-  // padding-right: 60upx;
-}
-.uni-navbar__placeholder {
-}
-.uni-navbar__placeholder-view {
-  height: $nav-height;
-}
 
-.uni-navbar--fixed {
-  position: fixed;
-  z-index: 998;
-}
+  }
 
-// .uni-navbar--shadow {
-//   box-shadow: 0 1px 6px #ccc;
-// }
+  .uni-navbar__content_view {
+    align-items: center;
+    flex-direction: row;
+  }
 
-// .uni-navbar--border:after {
-//   position: absolute;
-//   z-index: 3;
-//   bottom: 0;
-//   left: 0;
-//   right: 0;
-//   height: 1px;
-//   content: '';
-//   -webkit-transform: scaleY(0.5);
-//   transform: scaleY(0.5);
-//   background-color: $uni-border-color;
-// }
+  .uni-navbar__header {
+    flex-direction: row;
+    width: 750rpx;
+    height: $nav-height;
+    line-height: $nav-height;
+    font-size: 16px;
+  }
+
+  .uni-navbar__header-btns {
+    flex-wrap: nowrap;
+    width: 120upx;
+    padding: 0 12upx;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .uni-navbar__header-btns-left {
+    width: 150upx;
+    justify-content: flex-start;
+  }
+
+  .uni-navbar__header-btns-right {
+    width: 150upx;
+    // text-align: right;
+    padding-right: 30rpx;
+    justify-content: flex-end;
+  }
+
+  .uni-navbar__header-container {
+    flex: 1;
+    // margin: 0 10upx;
+  }
+
+  .uni-navbar__header-container-inner {
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    font-size: 30upx;
+  }
+
+  .uni-navbar__placeholder {}
+
+  .uni-navbar__placeholder-view {
+    height: $nav-height;
+  }
+
+  .uni-navbar--fixed {
+    position: fixed;
+    z-index: 998;
+  }
+
+  .uni-navbar--shadow {
+    box-shadow: 0 1px 6px #ccc;
+  }
+
+  .uni-navbar--border {
+    border-bottom: 1rpx solid $uni-border-color;
+  }
 </style>
