f686d2e916cad7064916e084eadfb681960b3a92
wangyaqi <wangyaqi988@163.com>
2019-09-23 17:15:27 +0800

    update: list-item右侧开槽

--- src/pages/list/list.vue
+++ src/pages/list/list.vue
@@ -10,6 +10,11 @@
     <view class="example-title">菜单列表</view>
     <uni-list>
       <uni-list-item title="列表右侧带箭头" />
+      <uni-list-item title="列表右侧自定义内容">
+        <template v-slot:right>
+          <text style="color: #999999;">提示</text>
+        </template>
+      </uni-list-item>
       <uni-list-item :show-badge="true" title="列表右侧带箭头 + 角标" badge-text="12" />
       <uni-list-item title="列表左侧带略缩图" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/uni.png" />
       <uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="列表左侧带扩展图标" />
