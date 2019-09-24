1f5aceb550e0e6fea48102226ef0313acda298b6
陈晨 <chen458679677@gmail.com>
2019-09-24 16:29:00 +0800

    fix:the init value out of range can't minus or plus issue

--- src/components/uni-number-box/uni-number-box.vue
+++ src/components/uni-number-box/uni-number-box.vue
@@ -1,106 +1,98 @@
 <template>
-  <view class="uni-numbox">
-    <view
-      :class="{'uni-numbox--disabled': inputValue <= min || disabled}"
-      class="uni-numbox__minus"
-      @click="_calcValue('minus')">-</view>
-    <input
-      :disabled="disabled"
-      v-model="inputValue"
-      class="uni-numbox__value"
-      type="number"
-      @blur="_onBlur">
-    <view
-      :class="{'uni-numbox--disabled': inputValue >= max || disabled}"
-      class="uni-numbox__plus"
-      @click="_calcValue('plus')">+</view>
-  </view>
+	<view class="uni-numbox">
+		<view :class="{'uni-numbox--disabled': inputValue <= min || disabled}" class="uni-numbox__minus" @click="_calcValue('minus')">-</view>
+		<input :disabled="disabled" v-model="inputValue" class="uni-numbox__value" type="number" @blur="_onBlur">
+		<view :class="{'uni-numbox--disabled': inputValue >= max || disabled}" class="uni-numbox__plus" @click="_calcValue('plus')">+</view>
+	</view>
 </template>
 <script>
-export default {
-  name: 'UniNumberBox',
-  props: {
-    value: {
-      type: [Number, String],
-      default: 1
-    },
-    min: {
-      type: Number,
-      default: 0
-    },
-    max: {
-      type: Number,
-      default: 100
-    },
-    step: {
-      type: Number,
-      default: 1
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    }
-  },
-  data () {
-    return {
-      inputValue: 0
-    }
-  },
-  watch: {
-    value (val) {
-      this.inputValue = +val
-    },
-    inputValue (newVal, oldVal) {
-      if (+newVal !== +oldVal) {
-        this.$emit('change', newVal)
-      }
-    }
-  },
-  created () {
-    this.inputValue = +this.value
-  },
-  methods: {
-    _calcValue (type) {
-      if (this.disabled) {
-        return
-      }
-      const scale = this._getDecimalScale()
-      let value = this.inputValue * scale
-      let step = this.step * scale
-      if (type === 'minus') {
-        value -= step
-      } else if (type === 'plus') {
-        value += step
-      }
-      if (value < this.min || value > this.max) {
-        return
-      }
-      this.inputValue = value / scale
-    },
-    _getDecimalScale () {
-      let scale = 1
-      // 浮点型
-      if (~~this.step !== this.step) {
-        scale = Math.pow(10, (this.step + '').split('.')[1].length)
-      }
-      return scale
-    },
-    _onBlur (event) {
-      let value = event.detail.value
-      if (!value) {
-        this.inputValue = 0
-        return
-      }
-      value = +value
-      if (value > this.max) {
-        value = this.max
-      } else if (value < this.min) {
-        value = this.min
-      }
-      this.inputValue = value
-    }
-  }
-}
+	export default {
+		name: 'UniNumberBox',
+		props: {
+			value: {
+				type: [Number, String],
+				default: 1
+			},
+			min: {
+				type: Number,
+				default: 0
+			},
+			max: {
+				type: Number,
+				default: 100
+			},
+			step: {
+				type: Number,
+				default: 1
+			},
+			disabled: {
+				type: Boolean,
+				default: false
+			}
+		},
+		data() {
+			return {
+				inputValue: 0
+			}
+		},
+		watch: {
+			value(val) {
+				this.inputValue = +val
+			},
+			inputValue(newVal, oldVal) {
+				if (+newVal !== +oldVal) {
+					this.$emit('change', newVal)
+				}
+			}
+		},
+		created() {
+			this.inputValue = +this.value
+		},
+		methods: {
+			_calcValue(type) {
+				if (this.disabled) {
+					return
+				}
+				const scale = this._getDecimalScale()
+				let value = this.inputValue * scale
+				let step = this.step * scale
+				if (type === 'minus') {
+					value -= step
+				} else if (type === 'plus') {
+					value += step
+				}
+				if ("minus" === type && value < this.min) {
+					return;
+				}
+				if ("plus" === type && value > this.max) {
+					return;
+				}
+				this.inputValue = value / scale
+			},
+			_getDecimalScale() {
+				let scale = 1
+				// 浮点型
+				if (~~this.step !== this.step) {
+					scale = Math.pow(10, (this.step + '').split('.')[1].length)
+				}
+				return scale
+			},
+			_onBlur(event) {
+				let value = event.detail.value
+				if (!value) {
+					this.inputValue = 0
+					return
+				}
+				value = +value
+				if (value > this.max) {
+					value = this.max
+				} else if (value < this.min) {
+					value = this.min
+				}
+				this.inputValue = value
+			}
+		}
+	}
 </script>
 <style lang="scss">
 	$numbox-btn-width:70upx;
