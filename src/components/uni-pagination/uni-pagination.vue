9ba5b804182954f208cdbc7a985e03ec3a0f9356
陈晨 <chen458679677@gmail.com>
2019-09-26 17:26:42 +0800

    fix:uni-pagination 替换

--- src/components/uni-pagination/uni-pagination.vue
+++ src/components/uni-pagination/uni-pagination.vue
@@ -3,19 +3,29 @@
     <view class="uni-pagination__btns">
       <view class="uni-pagination__btn" :class="currentIndex === 1 ? 'uni-pagination--disabled' : 'uni-pagination--enabled'"
         :hover-class="currentIndex === 1 ? '' : 'uni-pagination--hover'" :hover-start-time="20" :hover-stay-time="70"
-        @click="clickLeft"> <template v-if="showIcon===true || showIcon === 'true'">
-          <uni-icons color="#000" size="20" type="arrowleft" /> </template> <template v-else><text class="uni-pagination-child__btn">{{ prevText }}</text></template>
+        @click="clickLeft">
+        <template v-if="showIcon===true || showIcon === 'true'">
+          <uni-icons color="#000" size="20" type="arrowleft" />
+        </template>
+        <template v-else><text class="uni-pagination-child__btn">{{ prevText }}</text></template>
       </view>
       <view class="uni-pagination__btn" :class="currentIndex === maxPage ? 'uni-pagination--disabled' : 'uni-pagination--enabled'"
         :hover-class="currentIndex === maxPage ? '' : 'uni-pagination--hover'" :hover-start-time="20" :hover-stay-time="70"
-        @click="clickRight"> <template v-if="showIcon===true || showIcon === 'true'">
-          <uni-icons color="#000" size="20" type="arrowright" /> </template> <template v-else><text class="uni-pagination-child__btn">{{ nextText }}</text></template>
+        @click="clickRight">
+        <template v-if="showIcon===true || showIcon === 'true'">
+          <uni-icons color="#000" size="20" type="arrowright" />
+        </template>
+        <template v-else><text class="uni-pagination-child__btn">{{ nextText }}</text></template>
       </view>
     </view>
-    <view class="uni-pagination__num"> <text class="uni-pagination__num-current">{{ currentIndex }}/{{ maxPage || 0 }}</text>
+    <view class="uni-pagination__num">
+      <view class="uni-pagination__num-current">
+        <text class="uni-pagination__num-current-text" style="color:#007aff">{{ currentIndex }}</text><text class="uni-pagination__num-current-text">/{{ maxPage || 0 }}</text>
+      </view>
     </view>
   </view>
 </template>
+
 <script>
   import uniIcons from '@/components/uni-icons/uni-icons.vue'
   export default {
@@ -85,7 +95,7 @@
         if (Number(this.currentIndex) === this.maxPage) {
           return
         }
-        this.currentIndex += 1 
+        this.currentIndex += 1
         this.change('next')
       },
       change(e) {
@@ -97,106 +107,123 @@
     }
   }
 </script>
+
 <style lang="scss">
-  @import '@/uni.scss';
+	@import '@/uni.scss';
+  @mixin pagination-disabled {
+    opacity: 0.3;
+  }
+
+  @mixin pagination-hover {
+    color: rgba(0, 0, 0, .6);
+    background-color: $uni-bg-color-hover;
+  }
 
   /* 解决支付宝页面标签指定的样式覆盖组件内类名指定样式的BUG */
   /* #ifdef MP-ALIPAY */
-  page {
+    page {
+  /* #endif */
 
+  .uni-pagination {
+    /* #ifndef APP-PLUS-NVUE */
+      display: flex;
     /* #endif */
-    .uni-pagination {
-      /* #ifndef APP-PLUS-NVUE */
-      display: flex;
-      /* #endif */
-      /* #ifdef APP-PLUS-NVUE */
+    /* #ifdef APP-PLUS-NVUE */
       padding: 0 40rpx;
-      /* #endif */
-      width: 700rpx;
-      position: relative;
-      overflow: hidden;
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
-    }
-
-    .uni-pagination__btns {
-      /* #ifndef APP-PLUS-NVUE */
-      display: flex;
-      /* #endif */
-      flex: 1;
-      justify-content: space-between;
-      align-items: center;
-      flex-direction: row;
-    }
-
-    .uni-pagination__btn {
-      /* #ifndef APP-PLUS-NVUE */
-      display: flex;
-      /* #endif */
-      width: 120upx;
-      height: 60upx;
-      line-height: 60upx;
-      font-size: $uni-font-size-base;
-      position: relative;
-      background-color: #f8f8f8;
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
-      text-align: center;
-      border-width: 1rpx;
-      border-style: solid;
-      border-color: $uni-border-color;
-    }
-
-    .uni-pagination-child__btn {
-      /* #ifndef APP-PLUS-NVUE */
-      display: flex;
-      /* #endif */
-      font-size: $uni-font-size-base;
-      position: relative;
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
-      text-align: center;
-    }
-
-    .uni-pagination__num {
-      /* #ifndef APP-PLUS-NVUE */
-      display: flex;
-      /* #endif */
-      position: absolute;
-      left: 300rpx;
-      top: 0;
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
-      width: 100upx;
-      height: 60upx;
-      line-height: 60upx;
-      font-size: $uni-font-size-base;
-      color: $uni-text-color;
-    }
-
-    .uni-pagination__num-current {
-      // color: $uni-color-primary;
-    }
-
-    .uni-pagination--enabled {
-      color: #333333;
-      opacity: 1;
-    }
-
-    .uni-pagination--disabled {
-      opacity: 0.3;
-    }
-
-    .uni-pagination--hover {
-      color: rgba(0, 0, 0, .6);
-    }
-
-    /* #ifdef MP-ALIPAY */
+    /* #endif */
+    width: 700rpx;
+    position: relative;
+    overflow: hidden;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
   }
 
+  .uni-pagination__btns {
+    /* #ifndef APP-PLUS-NVUE */
+      display: flex;
+    /* #endif */
+    flex: 1;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+  }
+
+  .uni-pagination__btn {
+    /* #ifndef APP-PLUS-NVUE */
+      display: flex;
+    /* #endif */
+    width: 120upx;
+    height: 60upx;
+    line-height: 60upx;
+    font-size: $uni-font-size-base;
+    position: relative;
+    background-color: #f8f8f8;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    border-width: 1rpx;
+    border-style: solid;
+    border-color: $uni-border-color;
+  }
+
+  .uni-pagination-child__btn {
+    /* #ifndef APP-PLUS-NVUE */
+      display: flex;
+    /* #endif */
+    font-size: $uni-font-size-base;
+    position: relative;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  .uni-pagination__num {
+    /* #ifndef APP-PLUS-NVUE */
+      display: flex;
+    /* #endif */
+    position: absolute;
+    left: 300rpx;
+    top: 0;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100upx;
+    height: 60upx;
+    line-height: 60upx;
+    font-size: $uni-font-size-base;
+    color: $uni-text-color;
+  }
+
+  .uni-pagination__num-current {
+    // color: $uni-color-primary;
+    /* #ifndef APP-PLUS-NVUE */
+      display: flex;
+    /* #endif */
+    flex-direction: row;
+  }
+
+  .uni-pagination__num-current-text{
+    font-size: 15px;
+  }
+
+  .uni-pagination--enabled {
+    color: #333333;
+    opacity: 1;
+  }
+
+  .uni-pagination--disabled {
+    @include pagination-disabled;
+  }
+
+  .uni-pagination--hover {
+    @include pagination-hover;
+  }
+
+  /* #ifdef MP-ALIPAY */
+    }
   /* #endif */
+
 </style>
