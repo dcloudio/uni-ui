1769a8c4b0afdcad23826be661ed219e6becb638
wangyaqi <wangyaqi988@163.com>
2019-10-19 20:53:58 +0800

    update: 去除圆角

--- src/components/uni-count-down/uni-count-down.vue
+++ src/components/uni-count-down/uni-count-down.vue
@@ -181,7 +181,6 @@
 		width: $countdown-width;
 		height: $countdown-height;
 		line-height: $countdown-height;
-		border-radius: $uni-border-radius-base;
 		margin: 5rpx;
 		border-width: 1rpx;
 		border-style: solid;
