117bd77cb14fae2375eba781b1bab924f488b9cb
wangyaqi <wangyaqi988@163.com>
2019-12-03 10:52:15 +0800

    fix:修复手机浏览器Bug
    手机浏览器可能带bounce效果，会影响侧边索引滑动

--- src/components/uni-indexed-list/uni-indexed-list.vue
+++ src/components/uni-indexed-list/uni-indexed-list.vue
@@ -17,7 +17,7 @@
 			</cell>
 		</list>
 		<!-- #endif -->
-		<view :class="touchmove ? 'uni-indexed-list__menu--active' : ''" @touchstart="touchStart" @touchmove.stop="touchMove"
+		<view :class="touchmove ? 'uni-indexed-list__menu--active' : ''" @touchstart="touchStart" @touchmove.stop.prevent="touchMove"
 		 @touchend="touchEnd" class="uni-indexed-list__menu">
 			<view v-for="(list, key) in lists" :key="key" class="uni-indexed-list__menu-item">
 				<text class="uni-indexed-list__menu-text" :class="touchmoveIndex == key ? 'uni-indexed-list__menu-text--active' : ''">{{ list.key }}</text>
