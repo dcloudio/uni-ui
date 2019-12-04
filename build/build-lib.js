0936311896b67153ee65df702ec27ec789c08b0c
mehaotian <490272692@qq.com>
2019-12-04 15:34:53 +0800

    fix: 修改日历打开动画

--- build/build-lib.js
+++ build/build-lib.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra')
 const path = require('path')
-var glob = require("glob")
+const glob = require("glob")
+const exec = require('child_process').exec
 
 
 const packages = path.join(__dirname, '../src/components')
@@ -36,3 +37,27 @@ delFileLists.forEach((fileName) => {
 // fs.outputFileSync(path.join(lib, 'index.js'), outEvtCode, {
 // 	override: true
 // })
+start()
+function start () {
+  // 任何你期望执行的cmd命令，ls都可以
+  let cmdStr1 = 'npm publish'
+  let cmdPath = path.join(__dirname,'../lib')
+  // 子进程名称
+  let workerProcess
+  runExec(cmdStr1)
+  function runExec (cmdStr) {
+    workerProcess = exec(cmdStr, { cwd: cmdPath })
+    // 打印正常的后台可执行程序输出
+    workerProcess.stdout.on('data', function (data) {
+      console.log('stdout: ' + data)
+    })
+    // 打印错误的后台可执行程序输出
+    workerProcess.stderr.on('data', function (data) {
+      console.log('stderr: ' + data)
+    })
+    // 退出之后的输出
+    workerProcess.on('close', function (code) {
+        console.log(code)
+    })
+  }
+}
