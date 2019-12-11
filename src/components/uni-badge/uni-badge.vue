c0cf71fc21d887d2882aab616d5fc9acf622298b
mehaotian <490272692@qq.com>
2019-12-11 14:56:53 +0800

    fix: uni-calendar 新增 vue doc

--- src/components/uni-badge/uni-badge.vue
+++ src/components/uni-badge/uni-badge.vue
@@ -10,10 +10,17 @@
 	 * @tutorial https://ext.dcloud.net.cn/plugin?id=21
 	 * @property {String} text 角标内容
 	 * @property {String} type = [default|primary|success|warning|error] 颜色类型
+	 * 	@value default 灰色
+	 * 	@value primary 蓝色
+	 * 	@value success 绿色
+	 * 	@value warning 黄色
+	 * 	@value error 红色
 	 * @property {String} size = [normal|small] Badge 大小
+	 * 	@value normal 一般尺寸
+	 * 	@value small 小尺寸
 	 * @property {String} inverted = [true|false] 是否无需背景颜色
 	 * @event {Function} click 点击 Badge 触发事件
-	 * @example <uni-badge text="1"></uni-badge> 
+	 * @example <uni-badge text="1"></uni-badge>
 	 */
 	export default {
 		name: 'UniBadge',
