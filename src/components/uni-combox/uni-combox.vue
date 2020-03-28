42c7b73fd1d697bc04e979679c8505caaa9a895f
wangyaqi <wangyaqi988@163.com>
2020-03-28 17:36:15 +0800

    update: vue doc for combox

--- src/components/uni-combox/uni-combox.vue
+++ src/components/uni-combox/uni-combox.vue
@@ -23,6 +23,16 @@
 
 <script>
 	import uniIcons from '../uni-icons/uni-icons.vue'
+	/**
+	 * Combox 组合输入框
+	 * @description 组合输入框一般用于既可以输入也可以选择的场景
+	 * @property {String} label 左侧文字
+	 * @property {String} labelWidth 左侧内容宽度
+	 * @property {String} placeholder 输入框占位符
+	 * @property {Array} candidates 候选项列表
+	 * @property {String} emptyTips 筛选结果为空时显示的文字
+	 * @property {String} value 组合框的值
+	 */
 	export default {
 		name: 'uniCombox',
 		components: {
