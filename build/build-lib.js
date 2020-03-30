f2d716d8915742a68f9c65766bf89bc4ed895a75
mehaotian <490272692@qq.com>
2020-03-30 11:55:03 +0800

    update : build 逻辑 ，一键生成 lib 和 发布 npm

--- build/build-lib.js
+++ build/build-lib.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra')
 const path = require('path')
 const glob = require("glob")
 const exec = require('child_process').exec
-
+const argv = process.argv.splice(2)[0]
 
 const packages = path.join(__dirname, '../src/components')
 const lib = path.join(__dirname, '../lib')
@@ -11,52 +11,48 @@ const root = path.join(__dirname, '../')
 var filenames = []
 var filenamesUpper = []
 
-var files = glob.sync(packages + '/**/*.vue')
+fs.copy(packages, lib).then(() => {
+	console.log('---- 同步完成 ----');
+	const delFileLists = glob.sync(lib + '/**/*.{json,md,bak}')
+	// 删除json  md 等不必要文件
+	delFileLists.reduce((promise, fileName) => {
+		return promise.then(() => {
+			return fs.remove(fileName).then(() => {
+				console.log('删除成功', fileName);
+			})
+		})
+	}, Promise.resolve([])).then(() => {
+		// 删除所有文件成功之后，开始去同步 npm
+		if (argv === 'npm') {
+			start()
+		}
+	})
 
-// //复制vue文件到lib目录
-//   var relativePath = path.relative(packages, name)
-//   let fileName = relativePath.split('\\')[0]
-//   if (!~fileName.indexOf('uni-')) {
-//     return
-//   }
-//   const fileCatalog =fileName.split('/')
-//   const fileSubName = fileCatalog[1].split('.')[0]
-//   if(fileCatalog[0] === fileSubName){
-//     filenames.push(fileSubName)
-//   }
-//   var dest = path.join(lib, relativePath)
-
-fs.copySync(packages, lib)
-
-const delFileLists = glob.sync(lib + '/**/*.{json,md,bak}')
-// 删除json  md 等不必要文件
-delFileLists.forEach((fileName) => {
-	fs.removeSync(fileName)
 })
-// fs.outputFileSync(path.join(lib, 'index.js'), outEvtCode, {
-// 	override: true
-// })
-start()
-function start () {
-  // 任何你期望执行的cmd命令，ls都可以
-  let cmdStr1 = 'npm publish'
-  let cmdPath = path.join(__dirname,'..')
-  // 子进程名称
-  let workerProcess
-  runExec(cmdStr1)
-  function runExec (cmdStr) {
-    workerProcess = exec(cmdStr, { cwd: cmdPath })
-    // 打印正常的后台可执行程序输出
-    workerProcess.stdout.on('data', function (data) {
-      console.log('stdout: ' + data)
-    })
-    // 打印错误的后台可执行程序输出
-    workerProcess.stderr.on('data', function (data) {
-      console.log('stderr: ' + data)
-    })
-    // 退出之后的输出
-    workerProcess.on('close', function (code) {
-        console.log(code)
-    })
-  }
+
+function start() {
+	// 任何你期望执行的cmd命令，ls都可以
+	let cmdStr1 = 'npm publish'
+	let cmdPath = path.join(__dirname, '..')
+	let workerProcess = null
+	// 子进程名称
+	runExec(cmdStr1, cmdPath, workerProcess)
+}
+
+function runExec(cmdStr, cmdPath, workerProcess) {
+	workerProcess = exec(cmdStr, {
+		cwd: cmdPath
+	})
+	// 打印正常的后台可执行程序输出
+	workerProcess.stdout.on('data', function(data) {
+		console.log('stdout: ' + data)
+	})
+	// 打印错误的后台可执行程序输出
+	workerProcess.stderr.on('data', function(data) {
+		console.log('stderr: ' + data)
+	})
+	// 退出之后的输出
+	workerProcess.on('close', function(code) {
+		console.log(code)
+	})
 }
