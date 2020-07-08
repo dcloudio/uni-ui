904132e1af83ce40ee5e9696b0c9afde15fbbda0
mehaotian <490272692@qq.com>
2020-07-08 11:53:11 +0800

    fix : uni-rate 新增 disabled 属性，可设置组件禁用状态（之前版本的不可点击状态） fix :  uni-rate 新增 disabledColor 属性，可设置禁用颜色 fix : uni-rate 新增 readonly 属性，可设置组件只读属性 fix :  uni-rate 新增 allowHalf 属性，可设置组件是否开启半星选择 fix : uni-rate 新增 touchable 属性，可设置组件是否支持滑动手势 fix : uni-rate 修复 动态传值不更新的问题 fix : uni-rate 优化 value 属性可使用 v-model 双向绑定数据
    #132 #159

--- src/components/uni-rate/readme.md
+++ src/components/uni-rate/readme.md
@@ -1,53 +1,104 @@
-### Rate 评分
-*已经支持在nvue页面中使用*
+## Rate 评分
+> 组件名：``uni-rate``，代码块： uRate。
 
-评分组件，组件名：``uni-rate``，代码块： uRate。
+评分组件，多用于购买商品后，对商品进行评价等场景
 
-### 使用方式
+### 平台差异说明
 
-在 ``script`` 中引用组件 
+如无特殊说明，则全平台支持
 
-```javascript
-import uniRate from '@/components/uni-rate/uni-rate.vue'
-export default {
-    components: {uniRate}
-}
-```
+### 使用说明
 
-在 ``template`` 中使用组件
+为了避免错误使用，给大家带来不好的开发体验，请在使用组件前仔细阅读下面的使用说明，可以帮你避免一些必要的错误使用。
+
+- 组件内部依赖 `'uni-icons'` 组件
+- 组件需要依赖 `sass` 插件 ，请自行手动安装
+- 暂时不支持零星选择
+- 绑定值推荐使用 `v-model` 的方式
+
+
+### 基本用法 
 
 ```html
 <!-- 基本用法 -->
-<uni-rate value="2"></uni-rate>
+<!-- 需要在 script 中绑定 value 变量 -->
+<uni-rate v-model="value" @change="onChange"/>
 
-<!-- 自定义星星大小 -->
-<uni-rate size="18" value="5"></uni-rate>
+<!-- 不支持滑动手势选择评分 -->
+<uni-rate :touchable="false" :value="5"/>
+<!-- 设置尺寸大小 -->
+<uni-rate :size="18" :value="5"/>
 
 <!-- 设置评分数 -->
-<uni-rate max="10" value="5"></uni-rate>
+<uni-rate :max="10" :value="5" />
+	
+<!-- 设置星星间隔 -->
+<uni-rate :value="4" :margin="20" />	
 
-<!-- 不可点击状态 -->
-<uni-rate disabled="true" value="3.5"></uni-rate>
+<!-- 设置颜色 -->
+<uni-rate :value="3" color="#bbb" active-color="red" />
+
+<!-- 选择半星 -->
+<uni-rate allow-half :value="3.5" />
+
+<!-- 只读状态 -->
+<uni-rate :readonly="true" :value="2" />
+
+<!-- 禁用状态 -->
+<uni-rate :disabled="true" disabledColor="#ccc" :value="3" />
+
+<!-- 未选中的星星为镂空状态 -->
+<uni-rate :value="3" :is-fill="false" />
+
+			 
 ```
 
-### 属性说明
+```javascript
 
-|属性名			|类型	|默认值	|说明						|
-|---			|----	|---	|---						|
-|value			|Number	|0		|当前评分					|
-|max			|Number	|5		|最大的评分					|
-|size			|Number	|24		|星星的大小					|
-|margin			|Number	|0		|星星的间距					|
-|color			|String	|#ececec|星星的颜色					|
-|activeColor	|String	|#ffca3e|选中状态的星星的颜色		|
-|isFill		|Boolean|true	|星星的类型，是否为实心类型	|
-|disabled		|Boolean|false	|是否为不可点击状态			|
+export default {
+	components: {},
+	data() {
+		return {
+			value: 2
+		}
+	},
+	methods: {
+		onChange(e) {
+			console.log('rate发生改变:' + JSON.stringify(e))
+		}
+	}
+}
 
-### 事件说明
+```
 
-|事件称名	|说明								|返回参数			|
-|---		|----								|---				|
-|@change		|uniRate 的 value 改变时触发事件，	|e={value:Number}	|
+
+### uni-rate 属性说明
+
+属性名			|	类型			|	默认值	|	说明																									
+---				|	----		|	---		|	---	
+value/v-model	|	Number 	 	| 1			|	当前评分
+color 			|	String 	 	| #ececec	|	未选中状态的星星颜色
+activeColor 	|	String 	 	| #ffca3e	|	选中状态的星星颜色
+disabledColor 	|	String 	 	| #c0c0c0	|	禁用状态的星星颜色
+size 			|	Number 	 	| 24		|	星星的大小
+max 			|	Number 	 	| 5			|	最大评分评分数量，目前一分一颗星
+margin 			|	Number 	 	| 0			|	星星的间距，单位 px
+isFill 			| 	Boolean 	| true		|	星星的类型，是否为实心类型
+disabled 		|	Boolean 	| false		|	是否为禁用状态 (之前版本为已读状态，现更正为禁用状态)
+readonly 		|	Boolean 	| false		| 	是否为只读状态
+allowHalf		| 	Boolean 	| false		|	是否展示半星
+touchable		|	Boolean 	| true		|	是否支持滑动手势
+
+
+
+
+### uniListChat 事件说明
+
+事件称名			|	说明						|	返回参数			
+---				|	---						|	---	
+@change			|	改变 value 的值返回		|	e = { value:number }		
+
+
 
 ### 插件预览地址
 
--- src/components/uni-rate/uni-rate.vue
+++ src/components/uni-rate/uni-rate.vue
@@ -1,15 +1,44 @@
 <template>
-	<view class="uni-rate">
-		<view :key="index" :style="{ marginLeft: margin + 'px' }" @click="_onClick(index)" class="uni-rate__icon" v-for="(star, index) in stars">
-			<uni-icons :color="color" :size="size" :type="isFill ? 'star-filled' : 'star'" />
+	<view
+	    ref="uni-rate"
+	    class="uni-rate"
+	>
+		<view
+		    class="uni-rate__icon"
+		    :style="{ 'margin-right': margin + 'px' }"
+		    v-for="(star, index) in stars"
+		    :key="index"
+		    @touchstart.stop="touchstart"
+		    @touchmove.stop="touchmove"
+		>
+			<uni-icons
+			    :color="color"
+			    :size="size"
+			    :type="isFill ? 'star-filled' : 'star'"
+			/>
 			<!-- #ifdef APP-NVUE -->
-			<view :style="{ width: star.activeWitch.replace('%','')*size/100+'px'}" class="uni-rate__icon-on">
-				<uni-icons style="text-align: left;" :color="activeColor" :size="size" type="star-filled" />
+			<view
+			    :style="{ width: star.activeWitch.replace('%','')*size/100+'px'}"
+			    class="uni-rate__icon-on"
+			>
+				<uni-icons
+				    style="text-align: left;"
+				    :color="disabled?'#ccc':activeColor"
+				    :size="size"
+				    type="star-filled"
+				/>
 			</view>
 			<!-- #endif -->
 			<!-- #ifndef APP-NVUE -->
-			<view :style="{ width: star.activeWitch,top:-size/2+'px' }" class="uni-rate__icon-on">
-				<uni-icons :color="activeColor" :size="size" type="star-filled" />
+			<view
+			    :style="{ width: star.activeWitch,top:-size/2+'px' }"
+			    class="uni-rate__icon-on"
+			>
+				<uni-icons
+				    :color="disabled?disabledColor:activeColor"
+				    :size="size"
+				    type="star-filled"
+				/>
 			</view>
 			<!-- #endif -->
 		</view>
@@ -17,120 +46,35 @@
 </template>
 
 <script>
-	import uniIcons from "../uni-icons/uni-icons.vue";
-
+	import rateMixin from './mixins/rate-mixin.js'
 	/**
 	 * Rate 评分
 	 * @description 评分组件
 	 * @tutorial https://ext.dcloud.net.cn/plugin?id=33
-	 * @property {Number} value 当前评分
-	 * @property {Number} max 最大的评分
-	 * @property {Number} size 星星的大小
-	 * @property {Number} margin 星星的间距
-	 * @property {String} color 星星的颜色
-	 * @property {String} activeColor 选中状态的星星的颜色
-	 * @property {Boolean} isFill = [true|false] 星星的类型，是否为实心类型
-	 * @property {Boolean} disabled = [true|false] 是否为不可点击状态
-	 * @event {Function} change uniRate 的 value 改变时触发事件，e={value:Number}
+	 * @property {Boolean} 	isFill = [true|false] 		星星的类型，是否为实心类型, 默认为实心
+	 * @property {String} 	color 						未选中状态的星星颜色，默认为 "#ececec"
+	 * @property {String} 	activeColor 				选中状态的星星颜色，默认为 "#ffca3e"
+	 * @property {String} 	disabledColor 				禁用状态的星星颜色，默认为 "#c0c0c0"
+	 * @property {Number} 	size 						星星的大小
+	 * @property {Number} 	value/v-model 				当前评分
+	 * @property {Number} 	max 						最大评分评分数量，目前一分一颗星
+	 * @property {Number} 	margin 						星星的间距，单位 px
+	 * @property {Boolean} 	disabled = [true|false] 	是否为禁用状态，默认为 false
+	 * @property {Boolean} 	readonly = [true|false] 	是否为只读状态，默认为 false
+	 * @property {Boolean} 	allowHalf = [true|false] 	是否实现半星，默认为 false
+	 * @property {Boolean} 	touchable = [true|false] 	是否支持滑动手势，默认为 true
+	 * @event {Function} change 						uniRate 的 value 改变时触发事件，e={value:Number}
 	 */
-
 	export default {
 		name: "UniRate",
-		components: {
-			uniIcons
-		},
-		props: {
-			isFill: {
-				// 星星的类型，是否镂空
-				type: [Boolean, String],
-				default: true
-			},
-			color: {
-				// 星星的颜色
-				type: String,
-				default: "#ececec"
-			},
-			activeColor: {
-				// 星星选中状态颜色
-				type: String,
-				default: "#ffca3e"
-			},
-			size: {
-				// 星星的大小
-				type: [Number, String],
-				default: 24
-			},
-			value: {
-				// 当前评分
-				type: [Number, String],
-				default: 0
-			},
-			max: {
-				// 最大评分
-				type: [Number, String],
-				default: 5
-			},
-			margin: {
-				// 星星的间距
-				type: [Number, String],
-				default: 0
-			},
-			disabled: {
-				// 是否可点击
-				type: [Boolean, String],
-				default: false
-			}
-		},
-		data() {
-			return {
-				valueSync: ""
-			};
-		},
-		computed: {
-			stars() {
-				const value = this.valueSync ? this.valueSync : 0;
-				const starList = [];
-				const floorValue = Math.floor(value);
-				const ceilValue = Math.ceil(value);
-				// console.log("ceilValue: " + ceilValue);
-				// console.log("floorValue: " + floorValue);
-				for (let i = 0; i < this.max; i++) {
-					if (floorValue > i) {
-						starList.push({
-							activeWitch: "100%"
-						});
-					} else if (ceilValue - 1 === i) {
-						starList.push({
-							activeWitch: (value - floorValue) * 100 + "%"
-						});
-					} else {
-						starList.push({
-							activeWitch: "0"
-						});
-					}
-				}
-				// console.log("starList[4]: " + starList[4].activeWitch);
-				return starList;
-			}
-		},
-		created() {
-			this.valueSync = Number(this.value);
-		},
-		methods: {
-			_onClick(index) {
-				if (this.disabled) {
-					return;
-				}
-				this.valueSync = index + 1;
-				this.$emit("change", {
-					value: this.valueSync
-				});
-			}
-		}
+		mixins: [rateMixin]
 	};
 </script>
 
-<style lang="scss" scoped>
+<style
+    lang="scss"
+    scoped
+>
 	.uni-rate {
 		/* #ifndef APP-NVUE */
 		display: flex;
