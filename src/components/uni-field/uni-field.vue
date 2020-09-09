3ead44665ffcf3e8c75d3f00f7b6aad2b2f90ca9
tianjiaxing <tianjiaxing2016@163.com>
2020-09-09 10:07:23 +0800

    update uni-field

--- src/components/uni-field/uni-field.vue
+++ src/components/uni-field/uni-field.vue
@@ -3,25 +3,37 @@
 		<view class="uni-field-inner" :class="[type == 'textarea' ? 'uni-textarea-inner' : '', 'uni-label-postion-' + labelPosition]">
 			<view class="uni-label" :class="[required ? 'uni-required' : '']" :style="{
 				justifyContent: justifyContent, 
-				flex: labelPosition == 'left' ? `0 0 ${labelWidth}rpx` : '1'
+				flex: labelPosition == 'left' ? `0 0 ${labelWidth}px` : '1'
 			}">
-				<view class="uni-icon-wrap" v-if="icon">
-					<uni-icon size="32" :custom-style="iconStyle" :name="icon" :color="iconColor" class="uni-icon"></uni-icon>
+				<view class="uni-icon-wrap" v-if="leftIcon">
+                    <uni-icons size="16" :type="leftIcon" :color="iconColor" />
 				</view>
-				<slot name="icon"></slot>
-				<text class="uni-label-text" :class="[this.$slots.icon || icon ? 'uni-label-left-gap' : '']">{{ label }}</text>
+				<slot name="leftIcon"></slot>
+				<text class="uni-label-text" :class="[this.$slots.leftIcon || leftIcon ? 'uni-label-left-gap' : '']">{{ label }}</text>
 			</view>
 			<view class="fild-body">
 				<view class="uni-flex-1 uni-flex" :style="[inputWrapStyle]">
-					<textarea v-if="type == 'textarea'" class="uni-flex-1 uni-textarea-class" :style="[fieldStyle]" :value="value"
-					 :placeholder="placeholder" :placeholderStyle="placeholderStyle" :disabled="disabled" :maxlength="inputMaxlength"
-					 :focus="focus" :autoHeight="autoHeight" :fixed="fixed" @input="onInput" @blur="onBlur" @focus="onFocus" @confirm="onConfirm"
-					 @tap="fieldClick" />
+					<textarea 
+                        v-if="type == 'textarea'" 
+                        class="uni-flex-1 uni-textarea-class"
+                        :name="name"
+                        :value="value"
+					    :placeholder="placeholder" 
+                        :placeholderStyle="placeholderStyle"
+                        :disabled="disabled"
+                        :maxlength="inputMaxlength"
+					    :focus="focus"
+                        :autoHeight="autoHeight"
+                        @input="onInput"
+                        @blur="onBlur"
+                        @focus="onFocus"
+                        @confirm="onConfirm"
+					    @tap="fieldClick" />
 					<input
 						v-else
-						:style="[fieldStyle]"
 						:type="type"
 						class="uni-flex-1 uni-field__input-wrap"
+                        :name="name"
 						:value="value"
 						:password="password || this.type === 'password'"
 						:placeholder="placeholder"
@@ -37,10 +49,13 @@
 						@tap="fieldClick"
 					/>
 				</view>
-			</view>
+                <uni-icons :size="clearSize" v-if="clearable && value != '' && focused" type="clear" color="#c0c4cc" @click="onClear" />
+				<view class="uni-button-wrap"><slot name="right" /></view>
+                <uni-icons v-if="rightIcon" size="16" @click="rightIconClick" :type="rightIcon" color="#c0c4cc" :style="[rightIconStyle]" />
+        	</view>
 		</view>
 		<view v-if="errorMessage !== false && errorMessage != ''" class="uni-error-message" :style="{
-			paddingLeft: labelWidth + 'rpx'
+			paddingLeft: labelWidth + 'px'
 		}">{{ errorMessage }}</view>
 	</view>
 </template>
@@ -48,33 +63,33 @@
 <script>
 /**
  * field 输入框
- * @description 借助此组件，可以实现表单的输入， 有"text"和"textarea"类型的，此外，借助uView的picker和actionSheet组件可以快速实现上拉菜单，时间，地区选择等， 为表单解决方案的利器。
- * @tutorial https://www.uviewui.com/components/field.html
+ * @description 此组件可以实现表单的输入， 有"text"和"textarea"类型。
  * @property {String} type 输入框的类型（默认text）
- * @property {String} icon label左边的图标，限uView的图标名称
- * @property {Object} icon-style 左边图标的样式，对象形式
- * @property {Boolean} right-icon 输入框右边的图标名称，限uView的图标名称（默认false）
  * @property {Boolean} required 是否必填，左边您显示红色"*"号（默认false）
+ * @property {String} leftIcon label左边的图标，限uni-ui的图标名称
+ * @property {String} iconColor 左边通过icon配置的图标的颜色（默认#606266）
+ * @property {Boolean} rightIcon 输入框右边的图标名称，限uni-ui的图标名称（默认false）
  * @property {String} label 输入框左边的文字提示
- * @property {Boolean} password 是否密码输入方式(用点替换文字)，type为text时有效（默认false）
+ * @property {Number String} labelWidth label的宽度，单位px（默认65）
+ * @property {String} labelAlign label的文字对齐方式（默认left）
+ * @property {String} labelPosition label的文字的位置（默认left）
  * @property {Boolean} clearable 是否显示右侧清空内容的图标控件(输入框有内容，且获得焦点时才显示)，点击可清空输入框内容（默认true）
- * @property {Number String} label-width label的宽度，单位rpx（默认130）
- * @property {String} label-align label的文字对齐方式（默认left）
- * @property {Object} field-style 自定义输入框的样式，对象形式
- * @property {Number | String} clear-size 清除图标的大小，单位rpx（默认30）
- * @property {String} input-align 输入框内容对齐方式（默认left）
- * @property {Boolean} border-bottom 是否显示field的下边框（默认true）
- * @property {Boolean} border-top 是否显示field的上边框（默认false）
- * @property {String} icon-color 左边通过icon配置的图标的颜色（默认#606266）
- * @property {Boolean} auto-height 是否自动增高输入区域，type为textarea时有效（默认true）
- * @property {String Boolean} error-message 显示的错误提示内容，如果为空字符串或者false，则不显示错误信息
  * @property {String} placeholder 输入框的提示文字
- * @property {String} placeholder-style placeholder的样式(内联样式，字符串)，如"color: #ddd"
+ * @property {String} placeholderStyle placeholder的样式(内联样式，字符串)，如"color: #ddd"
+ * @property {Boolean} password 是否密码输入方式(用点替换文字)，type为text时有效（默认false）
  * @property {Boolean} focus 是否自动获得焦点（默认false）
- * @property {Boolean} fixed 如果type为textarea，且在一个"position:fixed"的区域，需要指明为true（默认false）
  * @property {Boolean} disabled 是否不可输入（默认false）
  * @property {Number String} maxlength 最大输入长度，设置为 -1 的时候不限制最大长度（默认140）
- * @property {String} confirm-type 设置键盘右下角按钮的文字，仅在type="text"时生效（默认done）
+ * @property {String} confirmType 设置键盘右下角按钮的文字，仅在type="text"时生效（默认done）
+ * @property {String Boolean} errorMessage 显示的错误提示内容，如果为空字符串或者false，则不显示错误信息
+ * @property {Number | String} clearSize 清除图标的大小，单位px（默认15）
+ * @property {Boolean} trim 是否自动去除两端的空格
+ * @property {String} name 表单域的属性名，在使用校验规则时必填
+ * @property {Array} rules 单行表单验证规则，接受一个数组
+ * 
+ * @property {Boolean} border-bottom 是否显示field的下边框（默认true）
+ * @property {Boolean} border-top 是否显示field的上边框（默认false）
+ * @property {Boolean} auto-height 是否自动增高输入区域，type为textarea时有效（默认true）
  * @event {Function} input 输入框内容发生变化时触发
  * @event {Function} focus 输入框获得焦点时触发
  * @event {Function} blur 输入框失去焦点时触发
@@ -86,12 +101,8 @@
 export default {
 	name:"uni-field",
 	props: {
-		icon: String,
+		leftIcon: String,
 		rightIcon: String,
-		// arrowDirection: {
-		// 	type: String,
-		// 	default: 'right'
-		// },
 		required: Boolean,
 		label: String,
 		password: Boolean,
@@ -99,17 +110,13 @@ export default {
 			type: Boolean,
 			default: true
 		},
-		// 左边标题的宽度单位rpx
+		// 左边标题的宽度单位px
 		labelWidth: {
 			type: [Number, String],
-			default: 130
+			default: 65
 		},
 		// 对齐方式，left|center|right
 		labelAlign: {
-			type: String,
-			default: 'left'
-		},
-		inputAlign: {
 			type: String,
 			default: 'left'
 		},
@@ -127,8 +134,8 @@ export default {
 		},
 		placeholder: String,
 		placeholderStyle: String,
-		focus: Boolean,
-		fixed: Boolean,
+        focus: Boolean,
+        name: String,
 		value: [Number, String],
 		type: {
 			type: String,
@@ -151,24 +158,10 @@ export default {
 			type: String,
 			default: 'left'
 		},
-		// 输入框的自定义样式
-		fieldStyle: {
-			type: Object,
-			default() {
-				return {}
-			}
-		},
 		// 清除按钮的大小
 		clearSize: {
 			type: [Number, String],
-			default: 30
-		},
-		// lable左边的图标样式，对象形式
-		iconStyle: {
-			type: Object,
-			default() {
-				return {}
-			}
+			default: 15
 		},
 		// 是否显示上边框
 		borderTop: {
@@ -195,13 +188,12 @@ export default {
 	computed: {
 		inputWrapStyle() {
 			let style = {};
-			style.textAlign = this.inputAlign;
 			// 判断lable的位置，如果是left的话，让input左边两边有间隙
 			if(this.labelPosition == 'left') {
-				style.margin = `0 8rpx`;
+				style.margin = `0 4px`;
 			} else {
 				// 如果lable是top的，input的左边就没必要有间隙了
-				style.marginRight = `8rpx`;
+				style.marginRight = `4px`;
 			}
 			return style;
 		},
@@ -245,7 +237,7 @@ export default {
 		onInput(event) {
 			let value = event.detail.value;
 			// 判断是否去除空格
-			if(this.trim) value = this.$u.trim(value);
+			if(this.trim) value = this.trimStr(value);
 			this.$emit('input', value);
 		},
 		onFocus(event) {
@@ -264,6 +256,7 @@ export default {
 			this.$emit('confirm', e.detail.value);
 		},
 		onClear(event) {
+            console.log('====== event =======', event)
 			this.$emit('input', '');
 		},
 		rightIconClick() {
@@ -272,7 +265,20 @@ export default {
 		},
 		fieldClick() {
 			this.$emit('click');
-		}
+        },
+        trimStr(str, pos = 'both') {
+            if (pos == 'both') {
+                return str.replace(/^\s+|\s+$/g, "");
+            } else if (pos == "left") {
+                return str.replace(/^\s*/, '');
+            } else if (pos == 'right') {
+                return str.replace(/(\s*$)/g, "");
+            } else if (pos == 'all') {
+                return str.replace(/\s+/g, "");
+            } else {
+                return str;
+            }
+        }
 	}
 };
 </script>
@@ -280,11 +286,11 @@ export default {
 <style lang="scss" scoped>
 	
 .uni-field {
-	font-size: 28rpx;
-	padding: 20rpx 28rpx;
+	font-size: 14px;
+	padding: 10px 14px;
 	text-align: left;
 	position: relative;
-	color: $uni-main-color;
+	color: #333;
 }
 
 .uni-field-inner {
@@ -297,9 +303,9 @@ export default {
 }
 
 .uni-textarea-class {
-	min-height: 96rpx;
-	width: auto;
-	font-size: 28rpx;
+	min-height: 48px;
+	// width: auto;
+	font-size: 14px;
 }
 
 .fild-body {
@@ -309,7 +315,7 @@ export default {
 }
 
 .uni-arror-right {
-	margin-left: 8rpx;
+	margin-left: 4px;
 }
 
 .uni-label-text {
@@ -317,7 +323,7 @@ export default {
 }
 
 .uni-label-left-gap {
-	margin-left: 6rpx;
+	margin-left: 3px;
 }
 
 .uni-label-postion-top {
@@ -326,8 +332,8 @@ export default {
 }
 
 .uni-label {
-	width: 130rpx;
-	flex: 1 1 130rpx;
+	width: 65px;
+	flex: 1 1 65px;
 	text-align: left;
 	position: relative;
 	display: flex;
@@ -337,9 +343,9 @@ export default {
 .uni-required::before {
 	content: '*';
 	position: absolute;
-	left: -16rpx;
+	left: -8px;
 	font-size: 14px;
-	color: $uni-type-error;
+	color: red;
 	height: 9px;
 	line-height: 1;
 }
@@ -347,8 +353,8 @@ export default {
 .uni-field__input-wrap {
 	position: relative;
 	overflow: hidden;
-	font-size: 28rpx;
-	height: 48rpx;
+	font-size: 14px;
+	height: 24px;
 	flex: 1;
 	width: auto;
 }
@@ -359,8 +365,8 @@ export default {
 }
 
 .uni-error-message {
-	color: $uni-type-error;
-	font-size: 26rpx;
+	color: red;
+	font-size: 13px;
 	text-align: left;
 }
 
@@ -369,11 +375,11 @@ export default {
 }
 
 .uni-input-class {
-	font-size: 28rpx;
+	font-size: 14px;
 }
 
 .uni-button-wrap {
-	margin-left: 8rpx;
+	margin-left: 4px;
 }
 
 /* start--Retina 屏幕下的 1px 边框--start */
@@ -406,7 +412,7 @@ export default {
 	width: 199.8%;
 	height: 199.7%;
 	transform: scale(0.5, 0.5);
-	border: 0 solid $uni-border-color;
+	border: 0 solid #666;
 	z-index: 2;
 }
 
@@ -435,4 +441,17 @@ export default {
 }
 /* end--Retina 屏幕下的 1px 边框--end */
 
+.uni-icon-wrap {
+    // padding-right: 6px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.uni-button-wrap {
+    // padding-right: 6px;
+    display: flex;
+    align-items: right;
+    justify-content: center;
+}
 </style>
---
+++ src/pages/field/field.vue
@@ -0,0 +1,67 @@
+<template>
+  <view>
+      <uni-field
+        label="姓名"
+        label-align="right"
+        placeholder="请填写name"
+      ></uni-field>
+      <uni-field
+        v-model="mobile"
+        label="手机号"
+        left-icon="camera"
+        iconColor="red"
+        label-position="left"
+        label-align="right"
+        placeholder="请填写手机号"
+        placeholderStyle="color: blue"
+        :required="false"
+        type="text"
+        :clearable="true"
+
+     >
+        <button v-if="true" slot="right" size="mini" type="success">发送验证码</button>
+     </uni-field>
+     <uni-field
+        v-model="weixin"
+        label="微信号"
+        left-icon="camera"
+        label-position="left"
+        label-align="right"
+        placeholder="请填写手机号"
+        placeholderStyle="color: blue"
+        :required="false"
+        :focus="true"
+        type="text"
+        :clearable="true"
+        :password="false" 
+        labelWidth="100"
+        :disabled="false"
+        maxlength="5"
+        confirmType="good"
+     />
+     <uni-field
+        type="textarea"
+        label="家庭地址"
+        placeholder="请填写详细住址"
+        label-align="right"
+     />
+  </view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            mobile: '139 9999 9999',
+            weixin: 'sskd'
+        }
+    }
+    
+}
+</script>
+
+<style>
+page {
+    background: #fff;
+}
+</style>
