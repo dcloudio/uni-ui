f109fd6b4152a0cdc7bba5c8ae15eee7a3049b06
mehaotian <490272692@qq.com>
2020-12-03 18:34:30 +0800

    fix: 修改 easyinput 节流问题

--- components/uni-easyinput/uni-easyinput.vue
+++ components/uni-easyinput/uni-easyinput.vue
@@ -23,7 +23,7 @@
 			</template>
 			<template v-else>
 				<uni-icons class="content-clear-icon" :class="{'is-textarea-icon':type==='textarea'}" type="clear" :size="clearSize"
-				 v-if="clearable && focused && val != '' " color="#c0c4cc" @click="onClear"></uni-icons>
+				 v-if="clearable && focused && val " color="#c0c4cc" @click="onClear"></uni-icons>
 			</template>
 		</view>
 	</view>
@@ -173,7 +173,7 @@
 
 		},
 		mounted() {
-			this.onInput = throttle(this.input, 500)
+			// this.onInput = throttle(this.input, 500)
 			this.$nextTick(() => {
 				this.focused = this.focus
 			})
@@ -202,7 +202,7 @@
 			onEyes() {
 				this.showPassword = !this.showPassword
 			},
-			input(event) {
+			onInput(event) {
 				let value = event.detail.value;
 				// 判断是否去除空格
 				if (this.trim) value = this.trimStr(value);
