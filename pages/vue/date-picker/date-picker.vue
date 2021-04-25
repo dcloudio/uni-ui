67f727ee16869018e037418b021bff90a3a68f61
tianjiaxing <tianjiaxing2016@163.com>
2021-04-25 11:12:11 +0800

    change date-picker demo

--- pages/vue/date-picker/date-picker.vue
+++ pages/vue/date-picker/date-picker.vue
@@ -1,9 +1,13 @@
 <template>
 	<view class="page">
 		<text class="example-info">可以同时选择日期和时间的选择器</text>
-		<uni-section :title="'基本用法：' + single" type="line"></uni-section>
+		<uni-section :title="'日期用法：' + single" type="line"></uni-section>
 		<view class="example-body">
 			<uni-date-picker v-model="single" />
+		</view>
+		<uni-section :title="'日期时间用法：' + datetimesingle" type="line"></uni-section>
+		<view class="example-body">
+			<uni-date-picker type="datetime" v-model="datetimesingle" />
 		</view>
 		<uni-section :title="'v-model用法：' + single" type="line"></uni-section>
 		<view class="example-body">
@@ -29,6 +33,7 @@
 		data() {
 			return {
 				single: '2010-02-3',
+				'datetimesingle': '2010-03-1 00:00:00',
 				range: ['2020-10-8',  '2021-4-20']
 			}
 		},
