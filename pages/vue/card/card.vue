7b9fede4dcf66c9d47378673c82d95140ef61a34
tianjiaxing <tianjiaxing2016@163.com>
2021-07-01 14:51:12 +0800

    uni-card:- 优化 图文卡片无图片加载时，提供占位图标 - 新增 header 插槽，自定义卡片头部（ 图文卡片 mode="style" 时，不支持） - 修复 thumbnail 不存在仍然占位的 bug

--- pages/vue/card/card.vue
+++ pages/vue/card/card.vue
@@ -4,37 +4,61 @@
 		<uni-section title="基础卡片" type="line"></uni-section>
 		<view class="example-body">
 			<uni-card @click="clickCard"><text class="content-box-text">这是一个基础卡片示例，内容较少，此示例不带边框阴影。</text></uni-card>
-			<uni-card title="标题文字" isShadow @click="clickCard"><text class="content-box-text">这是一个基础卡片示例，内容比较多，内容样式可自定义，卡片视图常用来显示完整独立的一段信息，比如一篇文章的预览图、作者信息、时间等，此示例带边框阴影。</text></uni-card>
-			<uni-card title="标题文字" extra="额外信息" isShadow note="Tips" @click="clickCard"><text class="content-box-text">这是一个相对比较完整的基础卡片示例，带有标题、额外信息以及底部信息，内容样式可自定义。</text></uni-card>
+			<uni-card title="标题文字" isShadow @click="clickCard"><text
+					class="content-box-text">这是一个基础卡片示例，内容比较多，内容样式可自定义，卡片视图常用来显示完整独立的一段信息，比如一篇文章的预览图、作者信息、时间等，此示例带边框阴影。</text>
+			</uni-card>
+			<uni-card title="标题文字" extra="额外信息" isShadow note="Tips" @click="clickCard"><text
+					class="content-box-text">这是一个相对比较完整的基础卡片示例，带有标题、额外信息以及底部信息，内容样式可自定义。</text></uni-card>
 		</view>
 		<uni-section title="通栏卡片" type="line"></uni-section>
 		<view class="example-body">
 			<view class="example-box">
-				<uni-card title="标题文字" :isFull="true" isShadow='true' note="额外信息" extra="额外信息" @click="clickCard"><text class="content-box-text">通栏卡片，左右上下没有间距，用户可自定义卡片距离等信息</text></uni-card>
+				<uni-card title="标题文字" :isFull="true" isShadow='true' note="额外信息" extra="额外信息" @click="clickCard"><text
+						class="content-box-text">通栏卡片，左右上下没有间距，用户可自定义卡片距离等信息</text></uni-card>
 			</view>
 		</view>
-
+		<uni-section title="插槽用法" type="line"></uni-section>
+		<view class="example-body">
+			<view class="example-box">
+				<uni-card title="标题文字" :isFull="true" isShadow='true' note="额外信息" extra="额外信息" @click="clickCard">
+					<template slot="header">
+						<view>slot header</view>
+					</template>
+					<text class="content-box-text">通栏卡片，左右上下没有间距，用户可自定义卡片距离等信息</text>
+					<template slot="footer">
+						<view>slot footer</view>
+					</template>
+				</uni-card>
+			</view>
+		</view>
 		<uni-section title="图文卡片" type="line"></uni-section>
 		<view class="example-body">
-			<uni-card :is-shadow="true" title="标题文字" mode="style" thumbnail="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/460d46d0-4fcc-11eb-8ff1-d5dcf8779628.png"
-			 extra="额外信息" note="true" @click="clickCard">
-				<text class="content-box-text">图文卡片支持传入一张图片，在最上方显示，标题作为图片描述，额外信息作为内容标题，通常作用为作者信息或发布时间描述，自行配置是否需要底部信息</text>
+			<uni-card :is-shadow="true" title="标题文字" mode="style"
+				thumbnail="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/460d46d0-4fcc-11eb-8ff1-d5dcf8779628.png"
+				extra="额外信息" note="true" @click="clickCard">
+				<text
+					class="content-box-text">图文卡片支持传入一张图片，在最上方显示，标题作为图片描述，额外信息作为内容标题，通常作用为作者信息或发布时间描述，自行配置是否需要底部信息</text>
 				<block slot="footer">
 					<view class="footer-box">
-						<view class="" @click.stop="footerClick('喜欢')"><text class="footer-box__item">喜欢</text></view>
-						<view class="" @click.stop="footerClick('评论')"><text class="footer-box__item">评论</text></view>
-						<view class="" @click.stop="footerClick('分享')"><text class="footer-box__item">分享</text></view>
+						<view class="" @click.stop="footerClick('喜欢')"><text class="footer-box__item">喜欢</text>
+						</view>
+						<view class="" @click.stop="footerClick('评论')"><text class="footer-box__item">评论</text>
+						</view>
+						<view class="" @click.stop="footerClick('分享')"><text class="footer-box__item">分享</text>
+						</view>
 					</view>
 				</block>
 			</uni-card>
 		</view>
 		<uni-section title="标题卡片" type="line"></uni-section>
 		<view class="example-body">
-			<uni-card :isShadow="true" title="标题内容" subTitle="副标题" mode="title" thumbnail="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/460d46d0-4fcc-11eb-8ff1-d5dcf8779628.png"
-			 extra="技术没有上限" note="true" @click="clickCard">
+			<uni-card :isShadow="true" title="标题内容" subTitle="副标题" mode="title"
+				thumbnail="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/460d46d0-4fcc-11eb-8ff1-d5dcf8779628.png"
+				extra="技术没有上限" note="true" @click="clickCard">
 				<view>
 					<view class="image-box">
-						<image class="image" mode="aspectFill" src="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/094a9dc0-50c0-11eb-b680-7980c8a877b8.jpg" />
+						<image class="image" mode="aspectFill"
+							src="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/094a9dc0-50c0-11eb-b680-7980c8a877b8.jpg" />
 					</view>
 					<view class="content-box">
 						<text class="content-box-text">标题卡片带有一个双标题头部，右侧为额外描述信息 ，内容可自定义实现</text>
