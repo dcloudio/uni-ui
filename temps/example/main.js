584e27788988264d39f99cb23090291e8377f865
mehaotian <490272692@qq.com>
2021-07-30 19:23:57 +0800

    update: 示例项目兼容vue3

--- temps/example/main.js
+++ temps/example/main.js
@@ -1,3 +1,5 @@
+
+// #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
 
@@ -9,3 +11,15 @@ const app = new Vue({
     ...App
 })
 app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+import App from './App.vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
