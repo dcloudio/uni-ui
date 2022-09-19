f27dfc178a793aed248a306d73bace46cae555b9
codexu <461229187@qq.com>
2022-09-19 15:38:41 +0800

    fix(uni-datetime-picker): 反向选择日期范围，日期显示异常

--- uni_modules/uni-datetime-picker/components/uni-datetime-picker/calendar.vue
+++ uni_modules/uni-datetime-picker/components/uni-datetime-picker/calendar.vue
@@ -551,8 +551,16 @@
 				this.cale.setMultiple(this.calendar.fullDate, true)
 				this.weeks = this.cale.weeks
 				this.tempSingleDate = this.calendar.fullDate
-				this.tempRange.before = this.cale.multipleStatus.before
-				this.tempRange.after = this.cale.multipleStatus.after
+				const beforeStatus = this.cale.multipleStatus.before
+				const beforeDate = new Date(this.cale.multipleStatus.before).getTime()
+				const afterDate = new Date(this.cale.multipleStatus.after).getTime()
+				if (beforeDate > afterDate && afterDate) {
+					this.tempRange.before = this.cale.multipleStatus.after
+					this.tempRange.after = this.cale.multipleStatus.before
+				} else {
+					this.tempRange.before = this.cale.multipleStatus.before
+					this.tempRange.after = this.cale.multipleStatus.after
+				}
 				this.change()
 			},
 			/**
