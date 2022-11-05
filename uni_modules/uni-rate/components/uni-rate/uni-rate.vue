a7fa08576f8bf04a7ef4c97beec33e2c9397159a
keep-foolish <815355586@qq.com>
2022-11-05 21:58:22 +0800

    _getSize函数内是异步执行，加入回调确保符合预期的执行顺序

--- uni_modules/uni-rate/components/uni-rate/uni-rate.vue
+++ uni_modules/uni-rate/components/uni-rate/uni-rate.vue
@@ -258,32 +258,34 @@
 			 * 获取星星个数
 			 */
 			_getRateCount(clientX) {
-				this._getSize()
-				const size = Number(this.size)
-				if (isNaN(size)) {
-					return new Error('size 属性只能设置为数字')
-				}
-				const rateMoveRange = clientX - this._rateBoxLeft
-				let index = parseInt(rateMoveRange / (size + this.marginNumber))
-				index = index < 0 ? 0 : index;
-				index = index > this.max ? this.max : index;
-				const range = parseInt(rateMoveRange - (size + this.marginNumber) * index);
-				let value = 0;
-				if (this._oldValue === index && !this.PC) return;
-				this._oldValue = index;
-				if (this.allowHalf) {
-					if (range > (size / 2)) {
-						value = index + 1
-					} else {
-						value = index + 0.5
+				const _this = this;
+				this._getSize(function() {
+					const size = Number(_this.size)
+					if (isNaN(size)) {
+						return new Error('size 属性只能设置为数字')
 					}
-				} else {
-					value = index + 1
-				}
-
-				value = Math.max(0.5, Math.min(value, this.max))
-				this.valueSync = value
-				this._onChange()
+					const rateMoveRange = clientX - _this._rateBoxLeft
+					let index = parseInt(rateMoveRange / (size + _this.marginNumber))
+					index = index < 0 ? 0 : index;
+					index = index > _this.max ? _this.max : index;
+					const range = parseInt(rateMoveRange - (size + _this.marginNumber) * index);
+					let value = 0;
+					if (_this._oldValue === index && !_this.PC) return;
+					_this._oldValue = index;
+					if (_this.allowHalf) {
+						if (range > (size / 2)) {
+							value = index + 1
+						} else {
+							value = index + 0.5
+						}
+					} else {
+						value = index + 1
+					}
+					
+					value = Math.max(0.5, Math.min(value, _this.max))
+					_this.valueSync = value
+					_this._onChange()
+				})
 			},
 
 			/**
@@ -300,7 +302,7 @@
 			/**
 			 * 获取星星距离屏幕左侧距离
 			 */
-			_getSize() {
+			_getSize(fn) {
 				// #ifndef APP-NVUE
 				uni.createSelectorQuery()
 					.in(this)
@@ -309,6 +311,7 @@
 					.exec(ret => {
 						if (ret) {
 							this._rateBoxLeft = ret[0].left
+							fn && fn()
 						}
 					})
 				// #endif
@@ -317,6 +320,7 @@
 					const size = ret.size
 					if (size) {
 						this._rateBoxLeft = size.left
+						fn && fn()
 					}
 				})
 				// #endif
