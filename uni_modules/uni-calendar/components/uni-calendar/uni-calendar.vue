f5072436a0ced43e613e01eab6e9c2f200f9976a
何艺麒 <i@heyiqi.cc>
2023-04-23 11:16:07 +0800

    fix(uni-calendar): monthSwitch事件错误

--- uni_modules/uni-calendar/components/uni-calendar/uni-calendar.vue
+++ uni_modules/uni-calendar/components/uni-calendar/uni-calendar.vue
@@ -326,13 +326,12 @@
 			backToday() {
 				const nowYearMonth = `${this.nowDate.year}-${this.nowDate.month}`
 				const date = this.cale.getDate(new Date())
-        const todayYearMonth = `${date.year}-${date.month}`
+                const todayYearMonth = `${date.year}-${date.month}`
+                this.init(date.fullDate)
 
-        if(nowYearMonth !== todayYearMonth) {
-          this.monthSwitch()
-        }
-
-				this.init(date.fullDate)
+                if(nowYearMonth !== todayYearMonth) {
+                this.monthSwitch()
+                }
 				this.change()
 			},
 			/**
