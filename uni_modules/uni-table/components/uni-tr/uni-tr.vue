9a13e3eaef026f048876d587d4e731287493878d
18148764734 <1938936680@qq.com>
2023-12-18 12:16:22 +0800

    修复 uni-tr只有一列时minWidth计算错误，列变化实时计算更新

--- uni_modules/uni-table/components/uni-tr/uni-tr.vue
+++ uni_modules/uni-table/components/uni-tr/uni-tr.vue
@@ -71,7 +71,7 @@ export default {
 	mounted() {
 		if (this.widthThArr.length > 0) {
 			const selectionWidth = this.selection === 'selection' ? 50 : 0
-			this.root.minWidth = this.widthThArr.reduce((a, b) => Number(a) + Number(b)) + selectionWidth
+			this.root.minWidth = Number(this.widthThArr.reduce((a, b) => Number(a) + Number(b))) + selectionWidth;
 		}
 	},
 	// #ifndef VUE3
@@ -90,7 +90,11 @@ export default {
 	// #endif
 	methods: {
 		minWidthUpdate(width) {
-			this.widthThArr.push(width)
+			this.widthThArr.push(width);
+			if (this.widthThArr.length > 0) {
+				const selectionWidth = this.selection === 'selection' ? 50 : 0;
+				this.root.minWidth = Number(this.widthThArr.reduce((a, b) => Number(a) + Number(b))) + selectionWidth;
+			};
 		},
 		// 选中
 		checkboxSelected(e) {
