4e10420705ca87ed7d80b0ea5b40649af22ae49f
mehaotian <490272692@qq.com>
2025-04-08 19:18:04 +0800

    fix: 新增鸿蒙支持 ，修复web控制台报错

--- uni_modules/uni-swipe-action/components/uni-swipe-action-item/uni-swipe-action-item.vue
+++ uni_modules/uni-swipe-action/components/uni-swipe-action-item/uni-swipe-action-item.vue
@@ -1,13 +1,13 @@
 <template>
 	<!-- 在微信小程序 app vue端 h5 使用wxs 实现-->
-	<!-- #ifdef APP-VUE || MP-WEIXIN || H5 -->
+	<!-- #ifdef APP-HARMONY || MP-WEIXIN || H5 -->
 	<view class="uni-swipe">
-		<!--  #ifdef MP-WEIXIN || VUE3 -->
+		<!--  #ifdef MP-WEIXIN || H5 -->
 		<view class="uni-swipe_box" :change:prop="wxsswipe.showWatch" :prop="is_show" :data-threshold="threshold"
 			:data-disabled="disabled" @touchstart="wxsswipe.touchstart" @touchmove="wxsswipe.touchmove"
 			@touchend="wxsswipe.touchend">
-		<!-- #endif -->
-			<!--  #ifndef MP-WEIXIN || VUE3 -->
+			<!-- #endif -->
+			<!--  #ifndef MP-WEIXIN || H5 -->
 			<view class="uni-swipe_box" :change:prop="renderswipe.showWatch" :prop="is_show" :data-threshold="threshold"
 				:data-disabled="disabled+''" @touchstart="renderswipe.touchstart" @touchmove="renderswipe.touchmove"
 				@touchend="renderswipe.touchend">
