bc376061d29a6d4abf93d58df2f471e20af11ddf
chouchouji <1305974212@qq.com>
2025-08-19 15:23:14 +0800

    fix(uni-datetime-picker): 修复ios端时间对比问题

--- uni_modules/uni-datetime-picker/components/uni-datetime-picker/util.js
+++ uni_modules/uni-datetime-picker/components/uni-datetime-picker/util.js
@@ -389,8 +389,8 @@ function getDefaultSecond(hideSecond) {
 }
 
 function dateCompare(startDate, endDate) {
-	startDate = new Date(fixIosDateFormat(startDate.trim()))
-	endDate = new Date(fixIosDateFormat(endDate.trim()))
+	startDate = new Date(fixIosDateFormat(typeof startDate === 'string' ? startDate.trim() : startDate))
+	endDate = new Date(fixIosDateFormat(typeof endDate === 'string' ? endDate.trim() : endDate))
 	return startDate <= endDate
 }
 
