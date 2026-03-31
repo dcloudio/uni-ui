5eb7ce1b1b7ccfc83ba6c8a9060a1e9fd0099752
chouchouji <jibinbin@dcloud.io>
2026-03-31 11:23:55 +0800

    test(fab-button): 添加测试用例

---
+++ pages/fab-button/fab-button.test.js
@@ -0,0 +1,17 @@
+const PAGE_PATH = '/pages/fab-button/fab-button'
+
+describe('fab-button', () => {
+	let page
+
+	beforeEach(async () => {
+		page = await program.reLaunch(PAGE_PATH)
+		await page.waitFor('view')
+	})
+
+    it('fab-button snapshot', async () => {
+        const image = await program.screenshot({
+            fullPage: true
+        })
+        expect(image).toSaveImageSnapshot()
+    })
+})
