a110a2a9bc0dddc23273a8927c7f628b55a1ba8e
chouchouji <jibinbin@dcloud.io>
2026-03-31 14:53:11 +0800

    test(collapse): 添加测试用例

---
+++ pages/collapse/collapse.test.js
@@ -0,0 +1,33 @@
+const PAGE_PATH = '/pages/collapse/collapse'
+
+describe('collapse', () => {
+	let page
+
+	beforeEach(async () => {
+		page = await program.reLaunch(PAGE_PATH)
+		await page.waitFor('scroll-view')
+	})
+
+	async function setPageData(newData) {
+		return await page.setData({
+			data: newData
+		});
+	}
+
+	it('collapse accordion', async () => {
+		expect(await page.data('data.lastEvent')).toBe('暂无交互')
+		expect(await page.data('data.enableOpen')).toBe(true)
+
+		await setPageData({
+			enableOpen: false
+		})
+		expect(await page.data('data.enableOpen')).toBe(false)
+	})
+
+	it('collapse snapshot', async () => {
+		const image = await program.screenshot({
+			fullPage: true
+		})
+		expect(image).toSaveImageSnapshot()
+	})
+})
