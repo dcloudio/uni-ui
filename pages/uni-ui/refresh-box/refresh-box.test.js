dd638fc887cbe94995c0986c05e020b5a686fb7a
chouchouji <jibinbin@dcloud.io>
2026-04-29 15:59:41 +0800

    test: 优化 refresh-box 测试用例

--- pages/uni-ui/refresh-box/refresh-box.test.js
+++ pages/uni-ui/refresh-box/refresh-box.test.js
@@ -2,6 +2,18 @@ const PAGE_PATH = '/pages/uni-ui/refresh-box/refresh-box'
 
 describe('refresh-box', () => {
   let page
+  const pullingSnapshotState = {
+    listCount1: 5,
+    pullingDistance1: 24,
+    listCount2: 5,
+    pullingDistance2: 60,
+    listCount3: 5,
+    refreshing3: true,
+    pullingDistance3: 60,
+    listCount4: 5,
+    refreshing4: true,
+    pullingDistance4: 36
+  }
 
   beforeEach(async () => {
     page = await program.reLaunch(PAGE_PATH)
@@ -14,16 +26,38 @@ describe('refresh-box', () => {
     })
   }
 
+  async function savePageSnapshot() {
+    await page.waitFor(200)
+
+    const image = await program.screenshot({
+      fullPage: true
+    })
+    expect(image).toSaveImageSnapshot()
+  }
+
   it('refresh-box state', async () => {
     expect(await page.data('data.listCount1')).toBe(3)
     expect(await page.data('data.refreshing1')).toBe(false)
     expect(await page.data('data.pullingDistance1')).toBe(0)
+    expect(await page.data('data.listCount2')).toBe(3)
+    expect(await page.data('data.refreshing2')).toBe(false)
+    expect(await page.data('data.pullingDistance2')).toBe(0)
+    expect(await page.data('data.listCount3')).toBe(3)
+    expect(await page.data('data.refreshing3')).toBe(false)
+    expect(await page.data('data.pullingDistance3')).toBe(0)
     expect(await page.data('data.listCount4')).toBe(3)
+    expect(await page.data('data.refreshing4')).toBe(false)
+    expect(await page.data('data.pullingDistance4')).toBe(0)
 
     await setPageData({
       listCount1: 8,
       refreshing1: true,
       pullingDistance1: 45,
+      listCount2: 4,
+      pullingDistance2: 50,
+      listCount3: 7,
+      refreshing3: true,
+      pullingDistance3: 60,
       listCount4: 6,
       refreshing4: true,
       pullingDistance4: 30
@@ -31,16 +65,20 @@ describe('refresh-box', () => {
 
     expect(await page.data('data.listCount1')).toBe(8)
     expect(await page.data('data.refreshing1')).toBe(true)
-    expect(await page.data('data.pullingDistance1')).toBe(45)
+    expect(await page.data('data.listCount2')).toBe(4)
+    expect(await page.data('data.refreshing2')).toBe(false)
+    expect(await page.data('data.listCount3')).toBe(7)
+    expect(await page.data('data.refreshing3')).toBe(true)
     expect(await page.data('data.listCount4')).toBe(6)
     expect(await page.data('data.refreshing4')).toBe(true)
-    // expect(await page.data('data.pullingDistance4')).toBe(30)
   })
 
-  it('refresh-box snapshot', async () => {
-    const image = await program.screenshot({
-      fullPage: true
-    })
-    expect(image).toSaveImageSnapshot()
+  it('refresh-box default snapshot', async () => {
+    await savePageSnapshot()
+  })
+
+  it('refresh-box pulling snapshot', async () => {
+    await setPageData(pullingSnapshotState)
+    await savePageSnapshot()
   })
 })
