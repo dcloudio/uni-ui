ba5a0dd27807fbbce9ca7da64f6763e17344ca3c
chouchouji <jibinbin@dcloud.io>
2026-08-28 10:38:44 +0800

    fix: app 非蒸汽模式不开放毛玻璃示例

--- pages/uni-ui/tab-bar/tab-bar.test.js
+++ pages/uni-ui/tab-bar/tab-bar.test.js
@@ -1,11 +1,22 @@
+const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
+const isAndroid = platformInfo.startsWith('android')
+const isIos = platformInfo.startsWith('ios')
+const isHarmony = platformInfo.startsWith('harmony')
+const isApp = isAndroid || isIos || isHarmony
+const isDom2 = process.env.UNI_APP_X_DOM2 === "true"
+
 const PAGE_PATHS = [
   '/pages/uni-ui/tab-bar/tab-bar',
   '/pages/uni-ui/tab-bar/tab-bar-dark',
   '/pages/uni-ui/tab-bar/tab-bar-custom',
-  '/pages/uni-ui/tab-bar/tab-bar-blur',
   '/pages/uni-ui/tab-bar/tab-bar-midbutton',
   '/pages/uni-ui/tab-bar/tab-bar-midbutton-notch'
 ]
+
+// app 非蒸汽模式不支持
+if (!(isApp && !isDom2)) {
+  PAGE_PATHS.push('/pages/uni-ui/tab-bar/tab-bar-blur')
+}
 
 describe('tab-bar', () => {
   let page
